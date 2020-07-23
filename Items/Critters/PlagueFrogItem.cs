89e973b28f4cf44df6f2ff1cafb031676af5ec55
willowmaine <68680944+willowmaine@users.noreply.github.com>
2020-07-23 14:57:15 +0800

    Add files via upload

---
+++ Items/Critters/PlagueFrogItem.cs
@@ -0,0 +1,45 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using CalValEX;
+using CalValEX.Items;
+using CalValEX.Items.Tiles;
+using Terraria.DataStructures;
+using System;
+using System.Collections.Generic;
+using CalValEX.Items.Critters;
+
+namespace CalValEX.Items.Critters
+{
+internal class PlagueFrogItem : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Plagued Frog");
+		}
+
+		public override void SetDefaults() {
+			item.useStyle = 1;
+			item.autoReuse = true;
+			item.useTurn = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.maxStack = 999;
+			item.consumable = true;
+			item.width = 22;
+			item.height = 18;
+			item.noUseGraphic = true;
+			item.bait = 15;
+			item.rare = 8;
+
+			Mod mod = ModLoader.GetMod("CalamityMod");
+            if (mod == null)
+            {
+                return;
+            }
+			item.makeNPC = (short)NPCType<PlagueFrog>();
+		}
+	}
+}
