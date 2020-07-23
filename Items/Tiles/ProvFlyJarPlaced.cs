303c9a7bd842aa0ad926cf7d7edb286843ee965b
willowmaine <68680944+willowmaine@users.noreply.github.com>
2020-07-24 01:09:41 +0800

    Add files via upload

---
+++ Items/Tiles/ProvFlyJarPlaced.cs
@@ -0,0 +1,45 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using static Terraria.ModLoader.ModContent;
+using CalValEX.Items.Tiles;
+
+namespace CalValEX.Items.Tiles
+{
+	class ProvFlyJarPlaced : ModTile
+	{
+		public override void SetDefaults() {
+			Main.tileFrameImportant[Type] = true;
+			Main.tileLighted[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.Width = 2;
+            TileObjectData.newTile.Height = 2;
+			TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16 }; //
+
+			animationFrameHeight = 38;
+            TileObjectData.addTile(Type);
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY) {
+			Item.NewItem(i * 16, j * 16, 48, 24, ItemType<ProvFlyJar>());
+		}
+
+		public override void AnimateTile(ref int frame, ref int frameCounter)
+        {
+            frameCounter++;
+            if (frameCounter > 8) //make this number lower/bigger for faster/slower animation
+            {
+                frameCounter = 0;
+                frame++;
+                if (frame > 7)
+                {
+                    frame = 0;
+                }
+            }
+        }
+	}
+}
