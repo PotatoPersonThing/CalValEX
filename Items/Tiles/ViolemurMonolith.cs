8135e97c1edfddcca9f6a47133df9b264f2940bf
willowmaine <68680944+willowmaine@users.noreply.github.com>
2020-07-24 23:17:44 +0800

    Violemur stuff

---
+++ Items/Tiles/ViolemurMonolith.cs
@@ -0,0 +1,48 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using CalValEX;
+using CalValEX.Items;
+using CalValEX.Items.Tiles;
+using CalValEX.Items.Critters;
+using Terraria.DataStructures;
+using System;
+using System.Collections.Generic;
+
+namespace CalValEX.Items.Tiles
+{
+    internal class ViolemurMonolith : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Violemur Perch");
+		}
+
+		public override void SetDefaults() {
+			item.useStyle = 1;
+			item.useTurn = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.autoReuse = true;
+			item.maxStack = 99;
+			item.consumable = true;
+			item.createTile = ModContent.TileType<ViolemurMonolithPlaced>();
+			item.width = 12;
+			item.height = 12;
+            item.rare = 4;
+		}
+
+public override void AddRecipes()
+    {
+    Mod CalValEX = ModLoader.GetMod("CalamityMod");
+            {
+                ModRecipe recipe = new ModRecipe(mod);
+        	recipe.AddIngredient(ModContent.ItemType<ViolemurItem>());
+         	recipe.AddIngredient(ModContent.ItemType<MonolithPot>());
+                recipe.SetResult(this);
+                recipe.AddRecipe();
+			}
+    }
+}}
---
+++ Items/Tiles/ViolemurStatue.cs
@@ -0,0 +1,44 @@
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Generation;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.World.Generation;
+using static Terraria.ModLoader.ModContent;
+using CalValEX.Items.Tiles;
+using CalValEX.Items.Critters;
+
+namespace CalValEX.Items.Tiles
+{
+
+	public class ViolemurStatue : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Violemur Statue");
+		}
+
+		public override void SetDefaults() {
+			item.CloneDefaults(ItemID.ArmorStatue);
+			item.createTile = TileType<ViolemurStatuePlaced>();
+			item.placeStyle = 0;
+		}
+
+        public override void AddRecipes()
+    {
+    Mod CalValEX = ModLoader.GetMod("CalamityMod");
+            {
+                ModRecipe recipe = new ModRecipe(mod);
+				recipe.AddIngredient(ModContent.ItemType<ViolemurItem>(), 5);
+                recipe.AddIngredient((ItemID.StoneBlock), 50);
+                recipe.AddTile(TileID.Anvils);
+                recipe.SetResult(this);
+                recipe.AddRecipe();
+			}
+    }
+	}
+}
