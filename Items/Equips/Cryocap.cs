170e331bb41085130b244f776b045e4c3bc2e012
willowmaine <68680944+willowmaine@users.noreply.github.com>
2020-07-26 18:55:32 +0800

    Cryon stuff

---
+++ Items/Equips/Cryocap.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CalValEX;
+using CalValEX.Items.Pets;
+using CalValEX.Items.Hooks;
+
+namespace CalValEX.Items.Equips
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class Cryocap : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Cryocap");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 26;
+            item.height = 14;
+            item.value = Item.sellPrice(0, 0, 3, 0);
+            item.rare = 5;
+            item.accessory = true;
+            item.vanity = true;
+        }
+          public virtual void DrawHair(ref bool drawHair, ref bool drawAltHair) 
+            {
+                drawHair = true;
+                drawAltHair = true;
+         } 
+         public override void ModifyTooltips(List<TooltipLine> tooltips)
+{
+    //rarity 12 (Turquoise) = new Color(0, 255, 200)
+    //rarity 13 (Pure Green) = new Color(0, 255, 0)
+    //rarity 14 (Dark Blue) = new Color(43, 96, 222)
+    //rarity 15 (Violet) = new Color(108, 45, 199)
+    //rarity 16 (Hot Pink/Developer) = new Color(255, 0, 255)
+    //rarity rainbow (no expert tag on item) = new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB)
+    //rarity rare variant = new Color(255, 140, 0)
+    //rarity dedicated(patron items) = new Color(139, 0, 0)
+    //look at https://calamitymod.gamepedia.com/Rarity to know where to use the colors
+    foreach (TooltipLine tooltipLine in tooltips)
+    {
+        if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
+        {
+            tooltipLine.overrideColor = new Color(0, 255, 200); //change the color accordingly to above
+        }
+    }
+}
+    }
+}
