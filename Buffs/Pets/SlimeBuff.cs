fe80f948617bdf2f8dc70c5d93240b16ebf188bb
PotatoPersonThing <68636419+PotatoPersonThing@users.noreply.github.com>
2020-07-26 19:55:20 -0700

    slime rework

--- Buffs/Pets/SlimeBuff.cs
+++ Buffs/Pets/SlimeBuff.cs
@@ -21,18 +21,6 @@ namespace CalValEX.Buffs.Pets {
 			if (petProjectileNotSpawned && player.whoAmI == Main.myPlayer) {
 				Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, ModContent.ProjectileType<Projectiles.Pets.Crimulan>(), 0, 0f, player.whoAmI, 0f, 0f);
 			}
-			player.buffTime[buffIndex] = 18000;
-			player.GetModPlayer<CalValEXPlayer>().eb = true;
-			bool petProjectileNotSpawnedf = player.ownedProjectileCounts[ModContent.ProjectileType<Projectiles.Pets.Ebonian>()] <= 0;
-			if (petProjectileNotSpawnedf && player.whoAmI == Main.myPlayer) {
-				Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, ModContent.ProjectileType<Projectiles.Pets.Ebonian>(), 0, 0f, player.whoAmI, 0f, 0f);
-			}
-            player.buffTime[buffIndex] = 18000;
-			player.GetModPlayer<CalValEXPlayer>().mSlime = true;
-			bool petProjectileNotSpawnedfl = player.ownedProjectileCounts[ModContent.ProjectileType<Projectiles.Pets.SlimeDemi>()] <= 0;
-			if (petProjectileNotSpawnedfl && player.whoAmI == Main.myPlayer) {
-				Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, ModContent.ProjectileType<Projectiles.Pets.SlimeDemi>(), 0, 0f, player.whoAmI, 0f, 0f);
-			}
 		}
 	}
 }
