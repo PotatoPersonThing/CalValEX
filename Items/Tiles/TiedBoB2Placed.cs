6cb235126370b5754802769d3ab70e5963c16c4e
willowmaine <68680944+willowmaine@users.noreply.github.com>
2020-07-30 15:30:04 +0800

    balons

---
+++ Items/Tiles/TiedBoB2Placed.cs
@@ -0,0 +1,49 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using static Terraria.ModLoader.ModContent;
+using CalValEX.Items.Tiles;
+
+namespace CalValEX.Items.Tiles
+{
+	class TiedBoB2Placed : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileLighted[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true; // Necessary since Style3x3Wall uses AnchorWall
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+			TileObjectData.newTile.Height = 3;
+			TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16 }; //
+	        animationFrameHeight = 54;
+            TileObjectData.addTile(Type);
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Tied Water Balloons");
+			AddMapEntry(new Color(255, 0, 251), name);
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			Item.NewItem(i * 16, j * 16, 16, 24, ItemType<TiedBoB2>());
+		}
+
+		public override void AnimateTile(ref int frame, ref int frameCounter)
+		{
+			frameCounter++;
+			if (frameCounter > 15) //make this number lower/bigger for faster/slower animation
+			{
+				frameCounter = 0;
+				frame++;
+				if (frame > 3)
+				{
+					frame = 0;
+				}
+			}
+		}
+	}
+}
+
