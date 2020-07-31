1391b5fbb716084b3e3e86607231cd78cf5e0784
PotatoPersonThing <68636419+PotatoPersonThing@users.noreply.github.com>
2020-07-31 09:31:24 -0700

    Ugh

--- Items/Pets/PristineExcalibur.cs
+++ Items/Pets/PristineExcalibur.cs
@@ -17,47 +17,17 @@ namespace CalValEX.Items.Pets
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Blursed Hilt");
+            DisplayName.SetDefault("Compensation");
             Tooltip
-                .SetDefault("I didn't get this off a Swamp Thing");
+                .SetDefault("Can be sold for a decent price");
         }
 
         public override void SetDefaults()
         {
-            item.CloneDefaults(ItemID.ZephyrFish);
-            item.UseSound = SoundID.Item105;
-            item.shoot = mod.ProjectileType("WraithsExcalibur");
-            item.value = Item.sellPrice(0, 1, 0, 0);
-            item.rare = 2;
-            item.buffType = mod.BuffType("Excalibuff");
+            item.value = Item.sellPrice(0, 50, 0, 0);
+            item.rare = -1;
         }
 
-        public override void ModifyTooltips(List<TooltipLine> tooltips)
-{
-    //rarity 12 (Turquoise) = new Color(0, 255, 200)
-    //rarity 13 (Pure Green) = new Color(0, 255, 0)
-    //rarity 14 (Dark Blue) = new Color(43, 96, 222)
-    //rarity 15 (Violet) = new Color(108, 45, 199)
-    //rarity 16 (Hot Pink/Developer) = new Color(255, 0, 255)
-    //rarity rainbow (no expert tag on item) = new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB)
-    //rarity rare variant = new Color(255, 140, 0)
-    //rarity dedicated(patron items) = new Color(139, 0, 0)
-    //look at https://calamitymod.gamepedia.com/Rarity to know where to use the colors
-    foreach (TooltipLine tooltipLine in tooltips)
-    {
-        if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
-        {
-            tooltipLine.overrideColor = new Color(255, 140, 0); //change the color accordingly to above
-        }
-    }
-}
 
-        public override void UseStyle(Player player)
-        {
-            if (player.whoAmI == Main.myPlayer && player.itemTime == 0)
-            {
-                player.AddBuff(item.buffType, 3600, true);
-            }
-        }
     }
 }
