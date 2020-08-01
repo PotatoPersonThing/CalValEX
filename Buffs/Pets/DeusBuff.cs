81454b66f9cf0873c83223efd57b2d8aa9b2a81c
PotatoPersonThing <68636419+PotatoPersonThing@users.noreply.github.com>
2020-07-31 19:55:33 -0700

    Worm heck buff

---
+++ Buffs/Pets/DeusBuff.cs
@@ -0,0 +1,45 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalValEX.Buffs.Pets
+{
+    public class DeusBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Astrum Demus");
+            Description.SetDefault("Wormheck");
+            Main.buffNoTimeDisplay[Type] = true;
+            Main.vanityPet[Type] = true;
+        }
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.buffTime[buffIndex] = 18000;
+            player.GetModPlayer<CalValEXPlayer>().deussmall = true;
+            bool petProjectileNotSpawned = player.ownedProjectileCounts[mod.ProjectileType("DeusSmallHead")] <= 0;
+            if (petProjectileNotSpawned && player.whoAmI == Main.myPlayer)
+            {
+                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, mod.ProjectileType("DeusSmallHead"), 0, 0f, player.whoAmI, 0f, 0f);
+                Projectile.NewProjectile(player.position.X + (float)(player.width * 2), player.position.Y + (float)(player.height / 2), 0f, 0f, mod.ProjectileType("DeusSmallHead"), 0, 0f, player.whoAmI, 0f, 0f);
+                Projectile.NewProjectile(player.position.X + (float)(player.width / 3), player.position.Y + (float)(player.height * 4), 0f, 0f, mod.ProjectileType("DeusSmallHead"), 0, 0f, player.whoAmI, 0f, 0f);
+                Projectile.NewProjectile(player.position.X + (float)(player.width * 3), player.position.Y + (float)(player.height * 5), 0f, 0f, mod.ProjectileType("DeusSmallHead"), 0, 0f, player.whoAmI, 0f, 0f);
+                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height * 7), 0f, 0f, mod.ProjectileType("DeusSmallHead"), 0, 0f, player.whoAmI, 0f, 0f);
+                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height * 9), 0f, 0f, mod.ProjectileType("DeusSmallHead"), 0, 0f, player.whoAmI, 0f, 0f);
+                Projectile.NewProjectile(player.position.X + (float)(player.width * 6), player.position.Y + (float)(player.height / 2), 0f, 0f, mod.ProjectileType("DeusSmallHead"), 0, 0f, player.whoAmI, 0f, 0f);
+                Projectile.NewProjectile(player.position.X + (float)(player.width * 10), player.position.Y + (float)(player.height / 2), 0f, 0f, mod.ProjectileType("DeusSmallHead"), 0, 0f, player.whoAmI, 0f, 0f);
+                Projectile.NewProjectile(player.position.X + (float)(player.width * 6), player.position.Y + (float)(player.height * 6), 0f, 0f, mod.ProjectileType("DeusSmallHead"), 0, 0f, player.whoAmI, 0f, 0f);
+                Projectile.NewProjectile(player.position.X + (float)(player.width * 10), player.position.Y + (float)(player.height * 10), 0f, 0f, mod.ProjectileType("DeusSmallHead"), 0, 0f, player.whoAmI, 0f, 0f);
+            }
+            player.buffTime[buffIndex] = 18000;
+            player.GetModPlayer<CalValEXPlayer>().deusmain = true;
+            bool petProjectileNotSpawned2 = player.ownedProjectileCounts[mod.ProjectileType("DeusHead")] <= 0;
+            if (petProjectileNotSpawned2 && player.whoAmI == Main.myPlayer)
+            {
+                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height * 20), 0f, 0f, mod.ProjectileType("DeusHead"), 0, 0f, player.whoAmI, 0f, 0f);
+            }
+        }
+    }
+
+}
