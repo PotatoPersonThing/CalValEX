05bb7fdf062a849daa3f273d91c4856dabc6d4fe
PotatoPersonThing <68636419+PotatoPersonThing@users.noreply.github.com>
2020-08-01 03:19:51 -0700

    Cursed lockpick

---
+++ Items/Pets/CursedLockpick.cs
@@ -0,0 +1,43 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using CalValEX;
+using CalValEX.Items;
+using CalValEX.Items.Hooks;
+using Terraria.DataStructures;
+using System;
+using System.Collections.Generic;
+
+namespace CalValEX.Items.Pets
+{
+    public class CursedLockpick : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Cursed Lockpick");
+            Tooltip
+                .SetDefault("Crusty");
+        }
+
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.ZephyrFish);
+            item.UseSound = SoundID.NPCHit4;
+            item.shoot = mod.ProjectileType("RustyMimic");
+            item.value = Item.sellPrice(0, 0, 10, 0);
+            item.rare = 3;
+            item.buffType = mod.BuffType("RustyBuff");
+        }
+
+        public override void UseStyle(Player player)
+        {
+            if (player.whoAmI == Main.myPlayer && player.itemTime == 0)
+            {
+                player.AddBuff(item.buffType, 3600, true);
+            }
+        }
+    }
+}
