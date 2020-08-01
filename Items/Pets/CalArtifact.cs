a8643672b290fee1b69451da160f481251360194
PotatoPersonThing <68636419+PotatoPersonThing@users.noreply.github.com>
2020-08-01 04:14:33 -0700

    Sepulcher item

---
+++ Items/Pets/CalArtifact.cs
@@ -0,0 +1,85 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using CalValEX;
+using CalValEX.Items;
+using CalValEX.Items.Hooks;
+using Terraria.DataStructures;
+using System;
+using System.Collections.Generic;
+using CalValEX.Items.Pets;
+
+namespace CalValEX.Items.Pets
+{
+    public class CalArtifact : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Calamitous Soul Artifact");
+            Tooltip.SetDefault("The grave rises");
+        }
+
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.ZephyrFish);
+            item.UseSound = SoundID.NPCHit4;
+            item.shoot = mod.ProjectileType("SepulcherHead");
+            item.value = Item.sellPrice(0, 20, 0, 0);
+            item.rare = 10;
+            item.buffType = mod.BuffType("SepulcherBuff");
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+{
+    //rarity 12 (Turquoise) = new Color(0, 255, 200)
+    //rarity 13 (Pure Green) = new Color(0, 255, 0)
+    //rarity 14 (Dark Blue) = new Color(43, 96, 222)
+    //rarity 15 (Violet) = new Color(108, 45, 199)
+    //rarity 16 (Hot Pink/Developer) = new Color(255, 0, 255)
+    //rarity rainbow (no expert tag on item) = new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB)
+    //rarity rare variant = new Color(255, 140, 0)
+    //rarity dedicated(patron items) = new Color(139, 0, 0)
+    //look at https://calamitymod.gamepedia.com/Rarity to know where to use the colors
+    foreach (TooltipLine tooltipLine in tooltips)
+    {
+        if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
+        {
+            tooltipLine.overrideColor = new Color(0, 255, 0); //change the color accordingly to above
+        }
+    }
+}
+
+        public override void UseStyle(Player player)
+        {
+            if (player.whoAmI == Main.myPlayer && player.itemTime == 0)
+            {
+                player.AddBuff(item.buffType, 3600, true);
+            }
+        }
+
+        public override bool Shoot(Player player, ref Microsoft.Xna.Framework.Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+                type = mod.ProjectileType("SepulcherHead");
+			return base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
+            }
+
+            public override void AddRecipes()
+        {
+        Mod CalValEX = ModLoader.GetMod("CalamityMod");
+                {
+                    ModRecipe recipe = new ModRecipe(mod);
+                    recipe.AddIngredient(mod.ItemType("Calacirclet"), 1);
+                    recipe.AddIngredient(ModLoader.GetMod("CalamityMod").ItemType("ExodiumClusterOre"), 15);
+                    recipe.AddIngredient(ModLoader.GetMod("CalamityMod").ItemType("Cinderplate"), 5);
+                    recipe.AddIngredient(ModLoader.GetMod("CalamityMod").ItemType("BloodstoneCore"), 10);
+                    recipe.AddTile(TileID.DemonAltar);
+                    recipe.SetResult(this);
+                    recipe.AddRecipe();
+                }
+        }
+	}
+}
+
