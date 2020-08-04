6a0825524def0f7ffff1999382a0bafb34087c97
PotatoPersonThing <68636419+PotatoPersonThing@users.noreply.github.com>
2020-08-04 05:26:48 -0700

    ortho

---
+++ Backgrounds/AstralBG.cs
@@ -0,0 +1,40 @@
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CalValEX.Backgrounds
+{
+	public class AstralBG : ModSurfaceBgStyle
+	{
+		public override bool ChooseBgStyle() {
+			return !Main.gameMenu && Main.LocalPlayer.GetModPlayer<CalValEXPlayer>().ZoneAstral;
+		}
+
+		// Use this to keep far Backgrounds like the mountains.
+		public override void ModifyFarFades(float[] fades, float transitionSpeed) {
+			for (int i = 0; i < fades.Length; i++) {
+				if (i == Slot) {
+					fades[i] += transitionSpeed;
+					if (fades[i] > 1f) {
+						fades[i] = 1f;
+					}
+				}
+				else {
+					fades[i] -= transitionSpeed;
+					if (fades[i] < 0f) {
+						fades[i] = 0f;
+					}
+				}
+			}
+		}
+
+		public override int ChooseCloseTexture(ref float scale, ref double parallax, ref float a, ref float b) {
+            b -= 1450f;
+			return mod.GetBackgroundSlot("Backgrounds/Astral");
+		}
+	}
+}
+
+
+
+
+
---
+++ CalValEXWorld.cs
@@ -0,0 +1,35 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Generation;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.World.Generation;
+using static Terraria.ModLoader.ModContent;
+using CalValEX.Items.Tiles;
+
+namespace CalValEX
+{
+	public class CalValEXWorld : ModWorld
+	{
+		public static int astralTiles;
+
+
+
+
+public override void ResetNearbyTileEffects() {
+			CalValEXPlayer modPlayer = Main.LocalPlayer.GetModPlayer<CalValEXPlayer>();
+			astralTiles = 0;
+		}
+
+		public override void TileCountsAvailable(int[] tileCounts) {
+			// Here we count various tiles towards ZoneAstral
+			astralTiles = tileCounts[TileType<Helplaced>()] + tileCounts[TileType<HesfinePlaced>()];
+        }
+    }
+}
