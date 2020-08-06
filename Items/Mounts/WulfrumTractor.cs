88e38133e3fa5815a58b9c51f4bfcbe5fc0dea64
PotatoPersonThing <68636419+PotatoPersonThing@users.noreply.github.com>
2020-08-06 02:15:21 -0700

    Wulfrum mount stuffs

--- Items/Mounts/WulfrumTractor.cs
+++ Items/Mounts/WulfrumTractor.cs
@@ -16,15 +16,15 @@ namespace CalValEX.Items.Mounts
 			mountData.buff = mod.BuffType("TractorMount");
 			mountData.heightBoost = 20;
 			mountData.fallDamage = 0.1f;
-			mountData.runSpeed = 8f;
-			mountData.dashSpeed = 5f;
+			mountData.runSpeed = 7f;
+			mountData.dashSpeed = 4f;
 			mountData.flightTimeMax = 0;
 			mountData.fatigueMax = 0;
 			mountData.jumpHeight = 8;
 			mountData.acceleration = 0.05f;
 			mountData.jumpSpeed = 4f;
 			mountData.blockExtraJumps = false;
-			mountData.totalFrames = 4;
+			mountData.totalFrames = 5;
 			mountData.constantJump = true;
 			int[] array = new int[mountData.totalFrames];
 			for (int l = 0; l < array.Length; l++) {
@@ -33,9 +33,9 @@ namespace CalValEX.Items.Mounts
 			mountData.playerYOffsets = array;
 			mountData.xOffset = 4;
 			mountData.bodyFrame = 3;
-			mountData.yOffset = 6;
+			mountData.yOffset = 14;
 			mountData.playerHeadOffset = 0;
-			mountData.standingFrameCount = 1;
+			mountData.standingFrameCount = 4;
 			mountData.standingFrameDelay = 12;
 			mountData.standingFrameStart = 0;
 			// mountData.runningFrameCount = 4;
@@ -51,8 +51,8 @@ namespace CalValEX.Items.Mounts
 			mountData.flyingFrameStart = 0;
 			mountData.inAirFrameCount = 1;
 			mountData.inAirFrameDelay = 12;
-			mountData.inAirFrameStart = 0;
-			mountData.idleFrameCount = 0;
+			mountData.inAirFrameStart = 4;
+			mountData.idleFrameCount = 4;
 			mountData.idleFrameDelay = 12;
 			mountData.idleFrameStart = 0;
 			mountData.idleFrameLoop = true;
@@ -73,10 +73,10 @@ namespace CalValEX.Items.Mounts
 			mountData.runningFrameCount = 4;
 			mountData.runningFrameDelay = 12;
 			mountData.runningFrameStart = 0;
-			mountData.inAirFrameCount = 0;
+			mountData.inAirFrameCount = 1;
 			mountData.inAirFrameDelay = 12;
-			mountData.inAirFrameStart = 0;
-			mountData.runSpeed = 8f;
+			mountData.inAirFrameStart = 4;
+			mountData.runSpeed = 7f;
 			mountData.acceleration = 0.05f;
 			}
             else 
@@ -84,10 +84,10 @@ namespace CalValEX.Items.Mounts
 			mountData.runningFrameCount = 4;
 			mountData.runningFrameDelay = 12;
 			mountData.runningFrameStart = 0;
-			mountData.inAirFrameCount = 0;
+			mountData.inAirFrameCount = 1;
 			mountData.inAirFrameDelay = 12;
-			mountData.inAirFrameStart = 0;
-			mountData.runSpeed = 8f; 
+			mountData.inAirFrameStart = 4;
+			mountData.runSpeed = 7f; 
 			}
 		}
 	}
