dbf11e58b26232aee23a740767d8d8a328b115d9
Kawaggy <emorei.game@gmail.com>
2020-08-07 21:22:28 +0100

    Fixed hat things
    1. Fixed hair not showing on Bandit Hat and Follyhat.
    2. Fixed Follyhat being equiped on the helmet slot instead of the vanity slot.

--- Items/Equips/BanditHat.cs
+++ Items/Equips/BanditHat.cs
@@ -1,35 +1,30 @@
-using System;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework;
 using Terraria;
-using Terraria.DataStructures;
-using Terraria.ID;
 using Terraria.ModLoader;
-using CalValEX;
-using CalValEX.Items.Pets;
-using CalValEX.Items.Hooks;
+using Terraria.ID;
 
 namespace CalValEX.Items.Equips
 {
-	[AutoloadEquip(EquipType.Head)]
-	public class BanditHat : ModItem
-	{
-        public override void SetStaticDefaults() 
-		{
-			DisplayName.SetDefault("Bandit's Hat");
-		}
-		public override void SetDefaults() {
-			item.width = 24;
-			item.height = 12;
-			item.rare = 3;
-			item.vanity = true;
-			item.value = Item.sellPrice(0, 3, 0, 0);
-		}
+    [AutoloadEquip(EquipType.Head)]
+    public class BanditHat : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Bandit's Hat");
+        }
 
-		public virtual void DrawHair(ref bool drawHair, ref bool drawAltHair)
-		{
-		drawHair = true;
-		drawAltHair = true;
-		}
-	}
+        public override void SetDefaults()
+        {
+            item.width = 24;
+            item.height = 12;
+            item.rare = ItemRarityID.Orange;
+            item.vanity = true;
+            item.value = Item.sellPrice(0, 3, 0, 0);
+        }
+
+        public override void DrawHair(ref bool drawHair, ref bool drawAltHair)
+        {
+            drawHair = true;
+            drawAltHair = true;
+        }
+    }
 }
--- Items/Equips/Birbhat.cs
+++ Items/Equips/Birbhat.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using System.Collections.Generic;
 using Terraria;
-using Terraria.DataStructures;
-using Terraria.ID;
 using Terraria.ModLoader;
-using CalValEX;
-using CalValEX.Items.Pets;
-using CalValEX.Items.Hooks;
+using Terraria.ID;
 
 namespace CalValEX.Items.Equips
 {
@@ -25,33 +20,25 @@ namespace CalValEX.Items.Equips
             item.width = 24;
             item.height = 28;
             item.value = Item.sellPrice(0, 1, 0, 0);
-            item.rare = 10;
-            item.accessory = true;
+            item.rare = ItemRarityID.Red;
             item.vanity = true;
         }
-          public virtual void DrawHair(ref bool drawHair, ref bool drawAltHair) 
-            {
-                drawHair = true;
-                drawAltHair = true;
-         } 
-         public override void ModifyTooltips(List<TooltipLine> tooltips)
-{
-    //rarity 12 (Turquoise) = new Color(0, 255, 200)
-    //rarity 13 (Pure Green) = new Color(0, 255, 0)
-    //rarity 14 (Dark Blue) = new Color(43, 96, 222)
-    //rarity 15 (Violet) = new Color(108, 45, 199)
-    //rarity 16 (Hot Pink/Developer) = new Color(255, 0, 255)
-    //rarity rainbow (no expert tag on item) = new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB)
-    //rarity rare variant = new Color(255, 140, 0)
-    //rarity dedicated(patron items) = new Color(139, 0, 0)
-    //look at https://calamitymod.gamepedia.com/Rarity to know where to use the colors
-    foreach (TooltipLine tooltipLine in tooltips)
-    {
-        if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
+
+        public override void DrawHair(ref bool drawHair, ref bool drawAltHair)
         {
-            tooltipLine.overrideColor = new Color(0, 255, 200); //change the color accordingly to above
+            drawHair = true;
+            drawAltHair = true;
         }
-    }
-}
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            foreach (TooltipLine tooltipLine in tooltips)
+            {
+                if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
+                {
+                    tooltipLine.overrideColor = new Color(0, 255, 200);
+                }
+            }
+        }
     }
 }
