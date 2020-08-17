0dd34f447241ed1609938c8d21fd0d1446d3921f
PotatoPersonThing <68636419+PotatoPersonThing@users.noreply.github.com>
2020-08-16 23:33:21 -0700

    Gun in vanity mod? unbelievable!

---
+++ Items/Tiles/AcidGun.cs
@@ -0,0 +1,54 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using CalValEX;
+using CalValEX.Items;
+using CalValEX.Items.Tiles;
+using Terraria.DataStructures;
+using System;
+using System.Collections.Generic;
+
+namespace CalValEX.Items.Tiles
+{
+	internal class AcidGun : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Acid Vine Gun");
+            Tooltip.SetDefault("Places an infinite amount of Sulphurous Vines");
+		}
+
+		public override void SetDefaults() {
+			item.useStyle = 1;
+			item.useTurn = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.autoReuse = true;
+			item.maxStack = 1;
+			item.width = 16;
+			item.height = 28;
+            item.rare = 4;
+			Mod calamityMod = ModLoader.GetMod("CalamityMod");
+			if (calamityMod != null)
+			{
+				item.createTile = (calamityMod.TileType("SulphurousVines"));
+			}
+		}
+
+        public override void AddRecipes()
+            {
+            Mod CalValEX = ModLoader.GetMod("CalamityMod");
+                    {
+                        ModRecipe recipe = new ModRecipe(mod);
+                        recipe.AddIngredient(ModLoader.GetMod("CalamityMod").ItemType("SulfuricScale"), 5);
+                        recipe.AddIngredient((ItemID.VineRope), 50);
+                        recipe.AddTile(TileID.Anvils);
+                        recipe.SetResult(this);
+                        recipe.AddRecipe();
+                    }
+            }
+	}
+}
