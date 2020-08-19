a80e7b65f4c53a6c60d60dd05da0c96e6a3e014c
willowmaine <68680944+willowmaine@users.noreply.github.com>
2020-08-19 11:22:45 +0800

    cleanup

--- Items/Termipebbles.cs
+++ Items/Termipebbles.cs
@@ -1,15 +1,7 @@
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using static Terraria.ModLoader.ModContent;
-using CalValEX;
-using CalValEX.Items;
-using CalValEX.Items.Tiles;
 using Terraria.DataStructures;
-using System;
-using System.Collections.Generic;
 
 namespace CalValEX.Items
 {
