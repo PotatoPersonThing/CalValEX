e4e0f5c6e77494d81c559374f110df72ba5499fb
willowmaine <68680944+willowmaine@users.noreply.github.com>
2020-08-19 18:29:09 +0800

    bruh

--- CalValEX.cs
+++ CalValEX.cs
@@ -1,13 +1,6 @@
-using System;
-using System.IO;
-using System.Linq;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System.Collections.Generic;
-using Terraria;
 using Terraria.ModLoader;
-using Terraria.ID;
 using Terraria.Localization;
+using CalValEX.Oracle;
 
 namespace CalValEX
 {
@@ -19,10 +12,12 @@ namespace CalValEX
 	{
 		Mod cal = ModLoader.GetMod("CalamityMod");
 			cal.GetItem("KnowledgeCrabulon").Tooltip.AddTranslation(GameCulture.English, "A crab and its mushrooms, a love story.\nIt's interesting how creatures can adapt given certain circumstances.\nFavorite this item to gain the Mushy buff while underground or in the mushroom biome.\nHowever, your movement speed will be decreased while in these areas due to you being covered in fungi.\nThe great crustacean's mushrooms may also grow angry when attacked, though they will also become harmless.");
-		
-	}
-	}
-	
-	
-		
+		 		Mod censusMod = ModLoader.GetMod("Census");
+ 	        if(censusMod != null)
+    		{
+                censusMod.Call("TownNPCCondition", ModContent.NPCType<OracleNPC>(), "Equip a Pet or Light Pet");
+
+            		}
+		}
+	}		
 }
