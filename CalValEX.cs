d6d62668d062f5379641baa1f5f87193c5e58cac
Kawaggy <emorei.game@gmail.com>
2020-08-21 23:08:34 +0100

    Nerfed ground mounts and fixed Cycle

--- CalValEX.cs
+++ CalValEX.cs
@@ -1,23 +1,43 @@
-using Terraria.ModLoader;
-using Terraria.Localization;
 using CalValEX.Oracle;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
 
 namespace CalValEX
 {
-	public class CalValEX : Mod
-	
-	{
-		public static bool Bumble;
-		public override void PostSetupContent()
-	{
-		Mod cal = ModLoader.GetMod("CalamityMod");
-			cal.GetItem("KnowledgeCrabulon").Tooltip.AddTranslation(GameCulture.English, "A crab and its mushrooms, a love story.\nIt's interesting how creatures can adapt given certain circumstances.\nFavorite this item to gain the Mushy buff while underground or in the mushroom biome.\nHowever, your movement speed will be decreased while in these areas due to you being covered in fungi.\nThe great crustacean's mushrooms may also grow angry when attacked, though they will also become harmless.");
-		 		Mod censusMod = ModLoader.GetMod("Census");
- 	        if(censusMod != null)
-    		{
-                censusMod.Call("TownNPCCondition", ModContent.NPCType<OracleNPC>(), "Equip a Pet or Light Pet");
+    public class CalValEX : Mod
+    {
+        public static bool Bumble;
 
-            		}
-		}
-	}		
+        public override void PostSetupContent()
+        {
+            Mod cal = ModLoader.GetMod("CalamityMod");
+            cal.GetItem("KnowledgeCrabulon").Tooltip.AddTranslation(GameCulture.English, "A crab and its mushrooms, a love story.\nIt's interesting how creatures can adapt given certain circumstances.\nFavorite this item to gain the Mushy buff while underground or in the mushroom biome.\nHowever, your movement speed will be decreased while in these areas due to you being covered in fungi.\nThe great crustacean's mushrooms may also grow angry when attacked, though they will also become harmless.");
+            Mod censusMod = ModLoader.GetMod("Census");
+            if (censusMod != null)
+            {
+                censusMod.Call("TownNPCCondition", ModContent.NPCType<OracleNPC>(), "Equip a Pet or Light Pet");
+            }
+        }
+
+        public static void MountNerf(Player player, float reduceDamageBy, float reduceHealthBy)
+        {
+            bool bossIsAlive = false;
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                NPC npc = Main.npc[i];
+                if (npc.active && npc.boss)
+                {
+                    bossIsAlive = true;
+                }
+            }
+
+            if (bossIsAlive)
+            {
+                int calculateLife = (int)(player.statLifeMax2 * reduceHealthBy);
+                player.statLifeMax2 -= calculateLife;
+                player.allDamage -= reduceDamageBy;
+            }
+        }
+    }
 }
