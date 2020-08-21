79c726a2f4510f45fe7a7c659a332ee7447e3e3c
PotatoPersonThing <68636419+PotatoPersonThing@users.noreply.github.com>
2020-08-21 01:56:34 -0700

    Necro

---
+++ Items/Tiles/Necrostone.cs
@@ -0,0 +1,29 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using CalValEX.Items.Tiles;
+
+namespace CalValEX.Items.Tiles
+{
+	public class Necrostone : ModItem
+	{
+		public override void SetStaticDefaults() {
+		}
+
+		public override void SetDefaults() {
+			item.width = 12;
+			item.height = 12;
+			item.maxStack = 999;
+            item.rare = 7;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.consumable = true;
+			item.createTile = ModContent.TileType<NecrostonePlaced>();
+		}
+	}
+}
