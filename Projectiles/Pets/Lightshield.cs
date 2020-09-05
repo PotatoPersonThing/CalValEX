2a8c599bf9dc47b99385f3a28030c8266909fee1
willowmaine <68680944+willowmaine@users.noreply.github.com>
2020-09-05 17:53:10 +0800

    shield

--- Projectiles/Pets/Lightshield.cs
+++ Projectiles/Pets/Lightshield.cs
@@ -1,12 +1,14 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
+using Terraria.Graphics.Shaders;
 using Terraria.ModLoader;
 
 namespace CalValEX.Projectiles.Pets
-    {
+{
 
-    public class Lightshield : FlyingPet
+    public class Lightshield : ModProjectile
     {
         public override void SetStaticDefaults()
         {
@@ -15,70 +17,60 @@ namespace CalValEX.Projectiles.Pets
             Main.projPet[projectile.type] = true;
         }
 
-        public override bool PreAI()
+        public override void SetDefaults()
         {
-            Player player = Main.player[projectile.owner];
-            return true;
-        }
-
-        public override void SafeSetDefaults() //SAFE SET DEFAULTS!!!
-        {
+            projectile.damage = 0;
+            projectile.friendly = true;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+            projectile.hostile = false;
             projectile.width = 98;
             projectile.height = 98;
-            projectile.ignoreWater = true;
-            projectile.tileCollide = true;
-            facingLeft = false; //is the sprite facing left? if so, put this to true. if its facing to right keep it false.
-            spinRotation = true; //should it spin? if that's the case, set to true. else, leave it false.
-            shouldFlip = false; //should the sprite flip? if that's the case, set to true. else, put it to false.
+            projectile.aiStyle = -1;
+            projectile.netImportant = true;
         }
 
-        public override void SetUpFlyingPet()
+        public override bool? CanCutTiles()
         {
-            distance[0] = -1f; //teleport distance
-            distance[1] = -1f; //faster speed distance
-            speed = -1f;
-            inertia = -1f;
-            animationSpeed = -1; //how fast the animation should play
-            spinRotationSpeedMult = 0.35f; //rotation speed multiplier, keep it positive for it to spin in the right direction
-            offSetX = projectile.Center.X - Main.screenPosition.X;
-            offSetY = projectile.Center.Y - Main.screenPosition.Y;
+            return false;
         }
 
-        public override void SetUpLight() //for when the pet emmits light
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
         {
-            shouldLightUp = true; //should the pet glow? true if it should, false if it shouldn't
-            RGB = new Vector3(108, 227, 255); //should only go up to 255 and as low as 0
-            intensity = 0.8f; //how intense the light should be. do not go over 2f or else the light will be too strong
-            abyssLightLevel = 2; //for abyss light level to work shouldLightUp must be set up true.
+            Lighting.AddLight(projectile.Center, 1.08f, 2.27f, 2.55f);
+            Texture2D texture = Main.projectileTexture[projectile.type];
+            spriteBatch.Draw(texture, projectile.Center - Main.screenPosition, null, projectile.GetAlpha(lightColor), projectile.rotation, new Vector2(texture.Width / 2, texture.Height / 2), 1f, projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.None, 0f);
+            return false;
         }
 
-        public override void SafeAI(Player player)
+        public override void AI()
         {
+            Player player = Main.player[projectile.owner];
+            if (player.dead || !player.active)
+                projectile.Kill();
+
+            Vector2 idlePosition = player.Center;
+            idlePosition.Y -= projectile.height / 2;
+            idlePosition.X -= projectile.width / 2;
+            projectile.position = idlePosition;
+            projectile.spriteDirection = 1;
+
+            projectile.rotation += -0.75f;
+            if (projectile.rotation > MathHelper.TwoPi)
+            {
+                projectile.rotation -= MathHelper.TwoPi;
+            }
+            else if (projectile.rotation < 0)
+            {
+                projectile.rotation += MathHelper.TwoPi;
+            }
+
             CalValEXPlayer modPlayer = player.GetModPlayer<CalValEXPlayer>();
 
             if (player.dead)
                 modPlayer.Lightshield = false;
             if (modPlayer.Lightshield)
                 projectile.timeLeft = 2;
-
-            /* THIS CODE ONLY RUNS AFTER THE MAIN CODE RAN.
-             * for custom behaviour, you can check if the projectile is walking or not via projectile.localAI[1]
-             * you should make new custom behaviour with numbers higher than 0, or less than 0
-             * the next few lines is an example on how to implement this
-             * 
-             * switch ((int)projectile.localAI[1])
-             * {
-             *     case -1:
-             *         break;
-             *     case 1:
-             *         break;
-             * }
-             * 
-             * 0 is already in use.
-             * 0 = flying
-             * 
-             * you can still use this, changing thing inside (however it's not recomended unless you want to add custom behaviour to this)
-             */
         }
     }
 }
