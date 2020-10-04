ca3f5f3fe1a473bf9f3dfbb0dbfeee9093ac145e
Kawaggy <emorei.game@gmail.com>
2020-10-04 16:17:24 +0100

    Remade Phantom in a Jar

--- Items/LightPets/PhantomJar.cs
+++ Items/LightPets/PhantomJar.cs
@@ -1,15 +1,8 @@
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using static Terraria.ModLoader.ModContent;
-using CalValEX;
-using CalValEX.Items;
-using CalValEX.Items.Hooks;
-using Terraria.DataStructures;
-using System;
-using System.Collections.Generic;
 
 namespace CalValEX.Items.LightPets
 {
@@ -24,33 +17,33 @@ namespace CalValEX.Items.LightPets
 
         public override void SetDefaults()
         {
-            item.CloneDefaults(3856);
+            item.CloneDefaults(ItemID.DD2PetGhost);
             item.UseSound = SoundID.NPCHit54;
             item.shoot = mod.ProjectileType("PhantomSpirit");
             item.value = Item.sellPrice(1, 30, 0, 0);
-            item.rare = 2;
+            item.rare = ItemRarityID.Green;
             item.buffType = mod.BuffType("PhantomBuff");
         }
 
         public override void ModifyTooltips(List<TooltipLine> tooltips)
-{
-    //rarity 12 (Turquoise) = new Color(0, 255, 200)
-    //rarity 13 (Pure Green) = new Color(0, 255, 0)
-    //rarity 14 (Dark Blue) = new Color(43, 96, 222)
-    //rarity 15 (Violet) = new Color(108, 45, 199)
-    //rarity 16 (Hot Pink/Developer) = new Color(255, 0, 255)
-    //rarity rainbow (no expert tag on item) = new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB)
-    //rarity rare variant = new Color(255, 140, 0)
-    //rarity dedicated(patron items) = new Color(139, 0, 0)
-    //look at https://calamitymod.gamepedia.com/Rarity to know where to use the colors
-    foreach (TooltipLine tooltipLine in tooltips)
-    {
-        if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
         {
-            tooltipLine.overrideColor = new Color(0, 255, 0); //change the color accordingly to above
+            //rarity 12 (Turquoise) = new Color(0, 255, 200)
+            //rarity 13 (Pure Green) = new Color(0, 255, 0)
+            //rarity 14 (Dark Blue) = new Color(43, 96, 222)
+            //rarity 15 (Violet) = new Color(108, 45, 199)
+            //rarity 16 (Hot Pink/Developer) = new Color(255, 0, 255)
+            //rarity rainbow (no expert tag on item) = new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB)
+            //rarity rare variant = new Color(255, 140, 0)
+            //rarity dedicated(patron items) = new Color(139, 0, 0)
+            //look at https://calamitymod.gamepedia.com/Rarity to know where to use the colors
+            foreach (TooltipLine tooltipLine in tooltips)
+            {
+                if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
+                {
+                    tooltipLine.overrideColor = new Color(0, 255, 0); //change the color accordingly to above
+                }
+            }
         }
-    }
-}
 
         public override void UseStyle(Player player)
         {
@@ -59,20 +52,18 @@ namespace CalValEX.Items.LightPets
                 player.AddBuff(item.buffType, 3600, true);
             }
         }
-                               public override void AddRecipes()
-		{
-			ModRecipe recipe = new ModRecipe(mod);
-               Mod calamityMod = ModLoader.GetMod("CalamityMod");
-            if (calamityMod != null)
-            {
-                recipe.AddIngredient((ItemID.WispinaBottle), 1);
-                recipe.AddIngredient(ModLoader.GetMod("CalamityMod").ItemType("RuinousSoul"), 3);
-                recipe.AddIngredient(ModLoader.GetMod("CalamityMod").ItemType("Phantoplasm"), 20);
-                recipe.AddTile(TileID.LunarCraftingStation);
-			    recipe.SetResult(this);
-			    recipe.AddRecipe();
-            }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            Mod calamityMod = ModLoader.GetMod("CalamityMod");
+
+            recipe.AddIngredient(ItemID.WispinaBottle);
+            recipe.AddIngredient(calamityMod.ItemType("RuinousSoul"), 3);
+            recipe.AddIngredient(calamityMod.ItemType("Phantoplasm"), 20);
+            recipe.AddTile(TileID.LunarCraftingStation);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
         }
-                    
     }
 }
--- Projectiles/Pets/LightPets/PhantomSpirit.cs
+++ Projectiles/Pets/LightPets/PhantomSpirit.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using CalValEX;
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.Graphics.Shaders;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -13,22 +14,22 @@ namespace CalValEX.Projectiles.Pets.LightPets
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Phantom Spirit");
-            Main.projFrames[projectile.type] = 6;
+            Main.projFrames[projectile.type] = 7;
             Main.projPet[projectile.type] = true;
-            base.drawOriginOffsetY = -15;
-			base.drawOffsetX = -14;
+            drawOriginOffsetY = -15;
+			drawOffsetX = -14;
         }
 
         public override void SetDefaults()
         {
-            projectile.CloneDefaults(702);
-            base.aiType = 702;
-        }
-
-        public override bool PreAI()
-        {
-            _ = Main.player[projectile.owner];
-            return true;
+            projectile.width = 16;
+            projectile.height = 16;
+            projectile.penetrate = -1;
+            projectile.netImportant = true;
+            projectile.timeLeft *= 5;
+            projectile.friendly = true;
+            projectile.tileCollide = false;
+            projectile.aiStyle = -1;
         }
 
         public override void AI()
@@ -36,17 +37,149 @@ namespace CalValEX.Projectiles.Pets.LightPets
             Player player = Main.player[projectile.owner];
             CalValEXPlayer modPlayer = player.GetModPlayer<CalValEXPlayer>();
             if (player.dead)
-            {
                 modPlayer.mPhan = false;
-            }
             if (modPlayer.mPhan)
-            {
                 projectile.timeLeft = 2;
-            }
-            if (Main.rand.NextFloat() < 0.5f)
+
+            Lighting.AddLight(projectile.position, new Vector3(2.5f, 1.20588235f, 1.54901961f));
+            float velocity = 2.5f;
+            float limit = 5f;
+
+            float extra = 240f;
+            float extra2 = 60f;
+
+            Vector2 extraPos = new Vector2(projectile.position.X + projectile.width * 0.5f, projectile.position.Y + projectile.height * 0.5f);
+            float posX = player.position.X + (player.width / 2) - extraPos.X;
+            float posY = player.position.Y + player.gfxOffY + (player.height / 2) - extraPos.Y;
+            if (player.controlLeft)
+                posX -= extra;
+            else if (player.controlRight)
+                posX += extra;
+
+            if (player.controlDown)
+                posY += extra;
+            else
             {
-                int dust = Dust.NewDust(projectile.Center, 89, 100, 66, 0f, 0f, 164, new Color(255,0,251), 1f);
-                Main.dust[dust].noGravity = true;
+                if (player.controlUp)
+                    posY -= extra;
+                posY -= extra2;
+            }
+
+            float num2 = (float)Math.Sqrt(posX * posX + posY * posY);
+
+            if (num2 > 1000f)
+            {
+                projectile.position.X += posX;
+                projectile.position.Y += posY;
+            }
+
+            if (projectile.localAI[0] == 1f)
+            {
+                if (num2 < 10f && Math.Abs(player.velocity.X) + Math.Abs(player.velocity.Y) < limit && player.velocity.Y == 0f)
+                    projectile.localAI[0] = 0f;
+
+                limit = 12f;
+                if (num2 < limit)
+                {
+                    projectile.velocity.X = posX;
+                    projectile.velocity.Y = posY;
+                }
+                else
+                {
+                    num2 = limit / num2;
+                    projectile.velocity.X = posX * num2;
+                    projectile.velocity.Y = posY * num2;
+                }
+
+                if (Main.rand.NextFloat() < 0.75f)
+                {
+                    int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 66, 0f, 0f, 164, new Color(255, 0, 251), 1f);
+                    Main.dust[dust].scale = 0.75f;
+                    Main.dust[dust].noGravity = true;
+                    Main.dust[dust].shader = GameShaders.Armor.GetSecondaryShader(player.cLight, player);
+                }
+
+                if (projectile.velocity.X > 0.5f)
+                    projectile.direction = -1;
+                else if (projectile.velocity.X < -0.5f)
+                    projectile.direction = 1;
+
+                projectile.spriteDirection = projectile.direction;
+                projectile.frame = 6;
+                projectile.rotation -= (0.2f + Math.Abs(projectile.velocity.X) * 0.025f) * projectile.direction;
+
+                return;
+            }
+
+            if (num2 > 350f)
+                projectile.localAI[0] = 1f;
+
+            if (projectile.velocity.X > 0.5f)
+                projectile.direction = -1;
+            else if (projectile.velocity.X < -0.5f)
+                projectile.direction = 1;
+
+            projectile.spriteDirection = projectile.direction;
+
+            if (num2 < 10f)
+            {
+                projectile.velocity.X = posX;
+                projectile.velocity.Y = posY;
+                projectile.rotation = projectile.velocity.X * 0.05f;
+                if (num2 < limit)
+                {
+                    projectile.position += projectile.velocity;
+                    projectile.velocity *= 0f;
+                    velocity = 0;
+                }
+
+                projectile.direction = -player.direction;
+            }
+
+            num2 = limit / num2;
+
+            posX *= num2;
+            posY *= num2;
+
+            if (projectile.velocity.X < posX)
+            {
+                projectile.velocity.X += velocity;
+                if (projectile.velocity.X < 0f)
+                    projectile.velocity.X *= 0.99f;
+            }
+
+            if (projectile.velocity.X > posX)
+            {
+                projectile.velocity.X -= velocity;
+                if (projectile.velocity.X > 0f)
+                    projectile.velocity.X *= 0.99f;
+            }
+
+            if (projectile.velocity.Y < posY)
+            {
+                projectile.velocity.Y += velocity;
+                if (projectile.velocity.Y < 0f)
+                    projectile.velocity.Y *= 0.99f;
+            }
+
+            if (projectile.velocity.Y > posY)
+            {
+                projectile.velocity.Y -= velocity;
+                if (projectile.velocity.Y > 0f)
+                    projectile.velocity.Y *= 0.99f;
+            }
+
+            if (projectile.velocity.X != 0f || projectile.velocity.Y != 0f)
+                projectile.rotation = projectile.velocity.X * 0.05f;
+
+            projectile.frameCounter++;
+            if (projectile.frameCounter > 5)
+            {
+                projectile.frame++;
+                projectile.frameCounter = 0;
+
+                if (projectile.frame > 5)
+                    projectile.frame = 0;
             }
         }
     }
