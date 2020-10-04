c7bae791548d757e32029acaa5b7d9b517960fcb
Kawaggy <emorei.game@gmail.com>
2020-10-04 18:19:24 +0100

    Minimpious rework

--- Items/LightPets/HolyTorch.cs
+++ Items/LightPets/HolyTorch.cs
@@ -1,15 +1,10 @@
+using CalValEX.Buffs.LightPets;
+using CalValEX.Projectiles.Pets.LightPets;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using static Terraria.ModLoader.ModContent;
-using CalValEX;
-using CalValEX.Items;
-using CalValEX.Items.Hooks;
-using Terraria.DataStructures;
-using System;
-using System.Collections.Generic;
 
 namespace CalValEX.Items.LightPets
 {
@@ -18,39 +13,38 @@ namespace CalValEX.Items.LightPets
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("The Holy Torch");
-            Tooltip
-                .SetDefault("Attracts an annoying fairy");
+            Tooltip.SetDefault("Attracts an annoying fairy");
         }
 
         public override void SetDefaults()
         {
             item.CloneDefaults(ItemID.ZephyrFish);
             item.UseSound = SoundID.NPCHit5;
-            item.shoot = mod.ProjectileType("ImpPet");
+            item.shoot = ModContent.ProjectileType<Minimpious>();
             item.value = Item.sellPrice(1, 0, 0, 0);
-            item.rare = 2;
-            item.buffType = mod.BuffType("ImpBuff");
+            item.rare = ItemRarityID.Green;
+            item.buffType = ModContent.BuffType<ImpBuff>();
         }
 
         public override void ModifyTooltips(List<TooltipLine> tooltips)
-{
-    //rarity 12 (Turquoise) = new Color(0, 255, 200)
-    //rarity 13 (Pure Green) = new Color(0, 255, 0)
-    //rarity 14 (Dark Blue) = new Color(43, 96, 222)
-    //rarity 15 (Violet) = new Color(108, 45, 199)
-    //rarity 16 (Hot Pink/Developer) = new Color(255, 0, 255)
-    //rarity rainbow (no expert tag on item) = new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB)
-    //rarity rare variant = new Color(255, 140, 0)
-    //rarity dedicated(patron items) = new Color(139, 0, 0)
-    //look at https://calamitymod.gamepedia.com/Rarity to know where to use the colors
-    foreach (TooltipLine tooltipLine in tooltips)
-    {
-        if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
         {
-            tooltipLine.overrideColor = new Color(0, 255, 200); //change the color accordingly to above
+            //rarity 12 (Turquoise) = new Color(0, 255, 200)
+            //rarity 13 (Pure Green) = new Color(0, 255, 0)
+            //rarity 14 (Dark Blue) = new Color(43, 96, 222)
+            //rarity 15 (Violet) = new Color(108, 45, 199)
+            //rarity 16 (Hot Pink/Developer) = new Color(255, 0, 255)
+            //rarity rainbow (no expert tag on item) = new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB)
+            //rarity rare variant = new Color(255, 140, 0)
+            //rarity dedicated(patron items) = new Color(139, 0, 0)
+            //look at https://calamitymod.gamepedia.com/Rarity to know where to use the colors
+            foreach (TooltipLine tooltipLine in tooltips)
+            {
+                if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
+                {
+                    tooltipLine.overrideColor = new Color(0, 255, 200); //change the color accordingly to above
+                }
+            }
         }
-    }
-}
 
         public override void UseStyle(Player player)
         {
