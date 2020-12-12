25bb0f3144e551dbce2260c9eb6698342498c058
GinYuH <72181395+GinYuH@users.noreply.github.com>
2020-12-12 00:25:59 -0800

    paintings ahoy!

---
+++ Items/Tiles/Paintings/UnholyTrip.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace CalValEX.Items.Tiles.Paintings
+{
+    public class UnholyTrip : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Unholy Trip");
+            Tooltip.SetDefault("'Mathew Maple'");
+        }
+
+        public override void SetDefaults()
+        {
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.maxStack = 99;
+            item.consumable = true;
+            item.createTile = ModContent.TileType<UnholyTripPlaced>();
+            item.width = 12;
+            item.height = 12;
+            item.rare = 10;
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            //rarity 12 (Turquoise) = new Color(0, 255, 200)
+            //rarity 13 (Pure Green) = new Color(0, 255, 0)
+            //rarity 14 (Dark Blue) = new Color(43, 96, 222)
+            //rarity 15 (Violet) = new Color(108, 45, 199)
+            //rarity 16 (Hot Pink/Developer) = new Color(255, 0, 255)
+            //rarity rainbow (no expert tag on item) = new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB)
+            //rarity rare variant = new Color(255, 140, 0)
+            //rarity dedicated(patron items) = new Color(139, 0, 0)
+            //look at https://calamitymod.gamepedia.com/Rarity to know where to use the colors
+            foreach (TooltipLine tooltipLine in tooltips)
+            {
+                if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
+                {
+                    tooltipLine.overrideColor = new Color(0, 255, 0); //change the color accordingly to above
+                }
+            }
+        }
+
+    }
+}
