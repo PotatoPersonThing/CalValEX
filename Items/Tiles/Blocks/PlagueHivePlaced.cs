47b7dddbb13f48464abd774273f1ecc8c8423f94
GinYuH <72181395+GinYuH@users.noreply.github.com>
2020-12-11 22:02:07 -0800

    Plague Wand

---
+++ Items/Tiles/Blocks/PlagueHivePlaced.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using CalValEX;
+using CalValEX.Items.Tiles;
+
+namespace CalValEX.Items.Tiles.Blocks
+{
+	public class PlagueHivePlaced : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileMergeDirt[Type] = false;
+			Main.tileBlockLight[Type] = true;
+			Main.tileLighted[Type] = true;
+			AddMapEntry(new Color(0, 76, 82));
+			dustType = 214;
+			minPick = 210;
+		}
+	}
+}
