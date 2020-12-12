4314b0d1caaea092c5b771f569d965b13ff6eccd
GinYuH <72181395+GinYuH@users.noreply.github.com>
2020-12-12 00:45:05 -0800

    fumouufufmou

---
+++ Projectiles/CalaFumo.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using CalValEX.Items;
+
+namespace CalValEX.Projectiles
+{
+    public class CalaFumo : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.netImportant = true;
+            projectile.width = 44;
+            projectile.height = 44;
+            projectile.aiStyle = 32;
+            projectile.friendly = true;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            Item.NewItem(projectile.getRect(), ModContent.ItemType<CalamitasFumo>());
+        }
+    }
+}
