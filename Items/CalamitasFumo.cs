d7367423626ad084f9f8dfd4b1587dae02085726
GinYuH <72181395+GinYuH@users.noreply.github.com>
2020-12-12 00:44:28 -0800

    fumofumo

---
+++ Items/CalamitasFumo.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using CalValEX;
+using CalValEX.Items;
+using CalValEX.Items.Hooks;
+using Terraria.DataStructures;
+using System;
+using System.Collections.Generic;
+
+namespace CalValEX.Items
+{
+    public class CalamitasFumo : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Calamitas Plushie");
+            Tooltip.SetDefault("A dark artifact that must be handled with care");
+        }
+
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.shootSpeed = 6f;
+            item.shoot = 155;
+            item.width = 44;
+            item.height = 44;
+            item.consumable = true;
+            item.UseSound = SoundID.Item1;
+            item.useAnimation = 20;
+            item.useTime = 20;
+            item.noUseGraphic = true;
+            item.noMelee = true;
+            item.value = 20;
+            item.shoot = mod.ProjectileType("CalaFumo");
+        }
+    }
+}
