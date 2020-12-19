f6d1d207e8dbae02ee9146ee26ae2f869f0b1108
GinYuH <72181395+GinYuH@users.noreply.github.com>
2020-12-18 23:55:45 -0800

    bed and bath and beyond

---
+++ Items/Tiles/FurnitureSets/Phantowax/PhantowaxBathtub.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using static Terraria.ModLoader.ModContent;
+using CalValEX.Items.Tiles;
+
+namespace CalValEX.Items.Tiles.FurnitureSets.Phantowax
+{
+	class PhantowaxBathtub : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileLighted[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style4x2);
+			TileObjectData.newTile.Width = 4;
+			TileObjectData.newTile.Height = 2;
+			TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16 }; //
+			TileObjectData.addTile(Type);
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Phantowax Bathtub");
+			AddMapEntry(new Color(94, 39, 93), name);
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			Item.NewItem(i * 16, j * 16, 64, 32, ItemType<PhantowaxBathtubItem>());
+		}
+	}
+}
---
+++ Items/Tiles/FurnitureSets/Phantowax/PhantowaxBathtubItem.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using CalValEX;
+using CalValEX.Items;
+using CalValEX.Items.Tiles;
+using Terraria.DataStructures;
+using System;
+using System.Collections.Generic;
+using CalValEX.Items.Tiles.FurnitureSets.Phantowax;
+
+namespace CalValEX.Items.Tiles.FurnitureSets.Phantowax
+{
+    internal class PhantowaxBathtubItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Phantowax Bathtub");
+        }
+
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.maxStack = 99;
+            item.consumable = true;
+            item.createTile = ModContent.TileType<PhantowaxBathtub>();
+            item.width = 12;
+            item.height = 12;
+            item.rare = 0;
+        }
+
+        public override void AddRecipes()
+        {
+            Mod CalValEX = ModLoader.GetMod("CalamityMod");
+            {
+                ModRecipe recipe = new ModRecipe(mod);
+                recipe.AddIngredient(ModContent.ItemType<PhantowaxBlock>(), 14);
+                recipe.AddTile(TileID.LunarCraftingStation);
+                recipe.SetResult(this);
+                recipe.AddRecipe();
+            }
+        }
+    }
+}
