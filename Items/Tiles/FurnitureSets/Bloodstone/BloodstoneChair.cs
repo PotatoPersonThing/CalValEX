bdc87be8d4f8f85c3c50e15f821562f515b9a83a
GinYuH <72181395+GinYuH@users.noreply.github.com>
2020-12-18 20:41:45 -0800

    blood chair flip

--- Items/Tiles/FurnitureSets/Bloodstone/BloodstoneChair.cs
+++ Items/Tiles/FurnitureSets/Bloodstone/BloodstoneChair.cs
@@ -1,42 +1,39 @@
+using CalValEX.Items.Tiles.FurnitureSets.Bloodstone;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Terraria;
+using Terraria.Enums;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
-using static Terraria.ModLoader.ModContent;
-using CalValEX.Items.Tiles;
 
 namespace CalValEX.Items.Tiles.FurnitureSets.Bloodstone
 {
-	class BloodstoneChair : ModTile
+	public class BloodstoneChair : ModTile
 	{
-		public override void SetDefaults()
-		{
+		public override void SetDefaults() {
 			Main.tileFrameImportant[Type] = true;
-			Main.tileLighted[Type] = true;
+			Main.tileNoAttach[Type] = true;
 			Main.tileLavaDeath[Type] = true;
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style1x2);
-			TileObjectData.newTile.Width = 1;
-			TileObjectData.newTile.Height = 2;
-			TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16 }; //
+			TileObjectData.newTile.CoordinateHeights = new[] { 16, 18 };
+			TileObjectData.newTile.Direction = TileObjectDirection.PlaceLeft;
+			TileObjectData.newTile.StyleWrapLimit = 2; //not really necessary but allows me to add more subtypes of chairs below the example chair texture
+			TileObjectData.newTile.StyleMultiplier = 2; //same as above
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+			TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceRight; //allows me to place example chairs facing the same way as the player
+			TileObjectData.addAlternate(1); //facing right will use the second texture style
 			TileObjectData.addTile(Type);
-            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsChair);
+			AddToArray(ref TileID.Sets.RoomNeeds.CountsAsChair);
 			ModTranslation name = CreateMapEntryName();
 			name.SetDefault("Bloodstone Chair");
 			AddMapEntry(new Color(139, 0, 0), name);
-            adjTiles = new int[] { TileID.Chairs };
+			disableSmartCursor = true;
+			adjTiles = new int[] { TileID.Chairs };
 		}
 
-        public override void SetSpriteEffects(int i, int j, ref SpriteEffects spriteEffects) {
-			if (i % 2 == 1) {
-				spriteEffects = SpriteEffects.FlipHorizontally;
-			}
-		}
-
-		public override void KillMultiTile(int i, int j, int frameX, int frameY)
-		{
-			Item.NewItem(i * 16, j * 16, 16, 32, ItemType<BloodstoneChairItem>());
+		public override void KillMultiTile(int i, int j, int frameX, int frameY) {
+			Item.NewItem(i * 16, j * 16, 16, 32, ModContent.ItemType<BloodstoneChairItem>());
 		}
 	}
 }
