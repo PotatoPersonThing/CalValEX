0e837deed06f4b2535822a268f6d7c2545ac1735
Kawaggy <emorei.game@gmail.com>
2020-12-30 16:49:19 +0000

    Saving/Loading of rescuedjelly

--- CalValEXWorld.cs
+++ CalValEXWorld.cs
@@ -1,27 +1,52 @@
-using Terraria;
-using Terraria.ModLoader;
-using static Terraria.ModLoader.ModContent;
 using CalValEX.Items.Tiles;
 using CalValEX.Items.Tiles.Astral;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using static Terraria.ModLoader.ModContent;
 
 namespace CalValEX
 {
-	public class CalValEXWorld : ModWorld
-	{
-		public static int astralTiles;
-		public static bool rescuedjelly = false;
+    public class CalValEXWorld : ModWorld
+    {
+        public static int astralTiles;
+        public static bool rescuedjelly;
 
-
-
-
-public override void ResetNearbyTileEffects() {
-			CalValEXPlayer modPlayer = Main.LocalPlayer.GetModPlayer<CalValEXPlayer>();
-			astralTiles = 0;
-		}
-
-		public override void TileCountsAvailable(int[] tileCounts) {
-			// Here we count various tiles towards ZoneAstral
-			astralTiles = tileCounts[TileType<Helplaced>()] + tileCounts[TileType<HesfinePlaced>()] + + tileCounts[TileType<AstralDirtPlaced>()];
+        public override void Initialize()
+        {
+            rescuedjelly = false;
         }
+
+        public override TagCompound Save()
+        {
+            var downed = new List<string>();
+            if (rescuedjelly) downed.Add("rescuedjelly");
+
+            return new TagCompound
+            {
+                ["downed"] = downed
+            };
+        }
+
+        public override void Load(TagCompound tag)
+        {
+            var downed = tag.GetList<string>("downed");
+            rescuedjelly = downed.Contains("rescuedjelly");
+        }
+
+        public override void ResetNearbyTileEffects()
+        {
+            astralTiles = 0;
+        }
+
+        public override void TileCountsAvailable(int[] tileCounts)
+        {
+            // Here we count various tiles towards ZoneAstral
+            astralTiles = tileCounts[TileType<Helplaced>()] + tileCounts[TileType<HesfinePlaced>()] + +tileCounts[TileType<AstralDirtPlaced>()];
+        }
+
+        public static void UpdateWorldBool() { if (Main.netMode == NetmodeID.Server) NetMessage.SendData(MessageID.WorldData); }
     }
 }
