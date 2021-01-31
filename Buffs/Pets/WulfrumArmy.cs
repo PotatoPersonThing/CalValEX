a98b31142d7489fb8fed0b56c08e6d11b8339f81
GinYuH <72181395+GinYuH@users.noreply.github.com>
2021-01-31 01:08:44 -0800

    Add files via upload

--- Buffs/Pets/WulfrumArmy.cs
+++ Buffs/Pets/WulfrumArmy.cs
@@ -41,12 +41,6 @@ namespace CalValEX.Buffs.Pets {
 			if (petProjectileNotSpawnedfgg && player.whoAmI == Main.myPlayer) {
 				Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, ModContent.ProjectileType<Projectiles.Pets.Wulfrum.WulfrumOrb>(), 0, 0f, player.whoAmI, 0f, 0f);
 			}
-            player.buffTime[buffIndex] = 18000;
-			player.GetModPlayer<CalValEXPlayer>().pylon = true;
-			bool petProjectileNotSpawnedddf = player.ownedProjectileCounts[ModContent.ProjectileType<Projectiles.Pets.Wulfrum.WulfrumPylon>()] <= 0;
-			if (petProjectileNotSpawnedddf && player.whoAmI == Main.myPlayer) {
-				Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, ModContent.ProjectileType<Projectiles.Pets.Wulfrum.WulfrumPylon>(), 0, 0f, player.whoAmI, 0f, 0f);
-			}
 		}
 	}
 }
