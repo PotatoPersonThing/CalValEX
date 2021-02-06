7ad08b813f67bc00462268d6f0fcbe7d9820dfdb
Kawaggy <emorei.game@gmail.com>
2021-02-06 04:41:12 +0000

    Added name, tooltip and recipe

--- Items/Equips/Hats/Draedon/DraedonHelmet.cs
+++ Items/Equips/Hats/Draedon/DraedonHelmet.cs
@@ -6,11 +6,29 @@ namespace CalValEX.Items.Equips.Hats.Draedon
 	[AutoloadEquip(EquipType.Head)]
 	public class DraedonHelmet : ModItem
 	{
-		public override void SetDefaults() {
+        public override void SetStaticDefaults()
+        {
+			DisplayName.SetDefault("Draedon's Helmet");
+			Tooltip.SetDefault("Changes appearance depending on held item damage type");
+        }
+
+        public override void SetDefaults()
+		{
 			item.width = 18;
 			item.height = 18;
 			item.rare = ItemRarityID.Blue;
 			item.vanity = true;
 		}
-	}
+
+        public override void AddRecipes()
+        {
+            Mod calamityMod = ModLoader.GetMod("CalamityMod");
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(calamityMod.ItemType("DubiousPlating"), 8);
+            recipe.AddIngredient(calamityMod.ItemType("MysteriousCircuitry"), 4);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
 }
