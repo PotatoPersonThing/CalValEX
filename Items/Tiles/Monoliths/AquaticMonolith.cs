779ba148ec0b11c454f9058ffaf42064ed564669
GinYuH <72181395+GinYuH@users.noreply.github.com>
2021-03-06 02:24:07 -0800

    some monolith stuff ig

---
+++ Items/Tiles/Monoliths/AquaticMonolith.cs
@@ -0,0 +1,24 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalValEX.Items.Tiles.Monoliths
+{
+	public class AquaticMonolith : ModItem
+	{
+		public override void SetDefaults() {
+			item.width = 22;
+			item.height = 32;
+			item.maxStack = 999;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.consumable = true;
+			item.rare = ItemRarityID.Yellow;
+			item.value = Item.buyPrice(0, 10, 0, 0);
+			item.createTile = ModContent.TileType<AquaticMonolithPlaced>();
+		}
+	}
+}
---
+++ Items/Tiles/Monoliths/PlagueMonolith.cs
@@ -0,0 +1,36 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalValEX.Items.Tiles.Monoliths
+{
+	public class PlagueMonolith : ModItem
+	{
+		public override void SetDefaults() {
+			item.width = 22;
+			item.height = 32;
+			item.maxStack = 999;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.consumable = true;
+			item.rare = ItemRarityID.Yellow;
+			item.value = Item.buyPrice(0, 10, 0, 0);
+			item.createTile = ModContent.TileType<PlagueMonolithPlaced>();
+		}
+
+        public override void AddRecipes()
+        {
+        Mod CalValEX = ModLoader.GetMod("CalamityMod");
+                {
+                    ModRecipe recipe = new ModRecipe(mod);
+                    recipe.AddIngredient(ModLoader.GetMod("CalamityMod").ItemType("PlagueCellCluster"), 15);
+                    recipe.AddTile(TileID.MythrilAnvil);
+                    recipe.SetResult(this);
+                    recipe.AddRecipe();
+                }
+        }
+	}
+}
