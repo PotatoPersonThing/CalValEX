d885f9e62c4289c33ce8aa01e44a6790589907f3
GinYuH <72181395+GinYuH@users.noreply.github.com>
2021-03-20 22:11:49 -0700

    Maple's bane

---
+++ Items/Tiles/FurnitureSets/Phantowax/PhantowaxDoorItem.cs
@@ -0,0 +1,40 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalValEX.Items.Tiles.FurnitureSets.Phantowax
+{
+    internal class PhantowaxDoorItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Phantowax Door");
+        }
+
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.maxStack = 99;
+            item.consumable = true;
+            item.createTile = ModContent.TileType<PhantowaxDoorClosed>();
+            item.width = 12;
+            item.height = 12;
+            item.rare = 0;
+        }
+
+        public override void AddRecipes()
+        {
+            Mod CalValEX = ModLoader.GetMod("CalamityMod");
+            {
+                ModRecipe recipe = new ModRecipe(mod);
+                recipe.AddIngredient(ModContent.ItemType<PhantowaxBlock>(), 6);
+                recipe.AddTile(TileID.LunarCraftingStation);
+                recipe.SetResult(this);
+                recipe.AddRecipe();
+            }
+        }
+    }
+}
