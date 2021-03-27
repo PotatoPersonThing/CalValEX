f344fa32434c84b51b917db5a1cf5d6777795ea1
GinYuH <72181395+GinYuH@users.noreply.github.com>
2021-03-26 20:01:46 -0700

    blight

---
+++ Items/Equips/Hats/AstrumDeusMask.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalValEX.Items.Equips.Hats
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class AstrumDeusMask : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Blighted Deus Mask");
+            Tooltip.SetDefault("'A faded star reignited.'");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 24;
+            item.height = 28;
+            item.value = Item.sellPrice(0, 1, 0, 0);
+            item.rare = ItemRarityID.Cyan;
+            item.vanity = true;
+        }
+
+        public override void DrawHair(ref bool drawHair, ref bool drawAltHair)
+        {
+            drawHair = false;
+        }
+    }
+}
