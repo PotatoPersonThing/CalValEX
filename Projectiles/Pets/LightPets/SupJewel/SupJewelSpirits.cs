a0df99df2431b6496fdc897a85a9a89442b21092
willowmaine <68680944+willowmaine@users.noreply.github.com>
2021-03-30 02:14:25 +0800

    Update SupJewelSpirits.cs

--- Projectiles/Pets/LightPets/SupJewel/SupJewelSpirits.cs
+++ Projectiles/Pets/LightPets/SupJewel/SupJewelSpirits.cs
@@ -5,19 +5,148 @@ using Terraria.ModLoader;
 
 namespace CalValEX.Projectiles.Pets.LightPets.SupJewel
 {
-    public class SpookShark : FlyingPet
+    public class NWings : ModProjectile
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Phantoshark");
+            DisplayName.SetDefault("Nightmare Fuel Wings");
+            Main.projFrames[projectile.type] = 4;
+            Main.projPet[projectile.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.damage = 0;
+            projectile.friendly = true;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+            projectile.hostile = false;
+            projectile.width = 154;
+            projectile.height = 200;
+            projectile.netImportant = true;
+            projectile.scale = 1f;
+        }
+
+        public override bool? CanCutTiles()
+        {
+            return false;
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            Lighting.AddLight(projectile.Center, new Vector3(0.541176471f, 1f, 1f));
+            Texture2D texture = Main.projectileTexture[projectile.type];
+            spriteBatch.Draw(texture, projectile.Center - Main.screenPosition, null, projectile.GetAlpha(lightColor), projectile.rotation, new Vector2(texture.Width / 2, texture.Height / 2), 1f, projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.None, 0f);
+            return false;
+        }
+
+        public override void AI()
+        {
+            Player player = Main.player[projectile.owner];
+
+            CalValEXPlayer modPlayer = player.GetModPlayer<CalValEXPlayer>();
+
+            if (player.dead)
+                modPlayer.SupJ = false;
+            if (modPlayer.SupJ)
+                projectile.timeLeft = 2;
+
+            Vector2 idlePosition = player.Center;
+            idlePosition.Y -= projectile.height / 2;
+            idlePosition.X -= projectile.width / 2;
+            projectile.position = idlePosition;
+            projectile.spriteDirection = 1;
+
+            projectile.frameCounter++;
+            if (projectile.frameCounter > 6)
+            {
+                projectile.frame++;
+                projectile.frameCounter = 0;
+            }
+            if (projectile.frame == 3)
+            {
+                projectile.frame = 0;
+            }
+
+            Mod calamityMod = ModLoader.GetMod("CalamityMod");
+            if (calamityMod != null)
+            {
+                calamityMod.Call("AddAbyssLightStrength", projectile.owner, 2);
+            }
+        }
+    }
+
+    public class Bishop : FlyingPet
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Dark Sun Elemental");
             Main.projFrames[projectile.type] = 4;
             Main.projPet[projectile.type] = true;
         }
 
         public override void SafeSetDefaults()
         {
-            projectile.width = 38;
-            projectile.height = 19;
+            projectile.width = 100;
+            projectile.height = 174;
+            projectile.ignoreWater = true;
+            facingLeft = true;
+            spinRotation = false;
+            shouldFlip = false;
+            usesAura = false;
+            usesGlowmask = true;
+            auraUsesGlowmask = false;
+        }
+
+        public override void SetUpFlyingPet()
+        {
+            distance[0] = 3400f; //teleport distance
+            distance[1] = 640f; //faster speed distance
+            speed = 4;
+            inertia = 70f;
+            animationSpeed = 6; //how fast the animation should play
+            spinRotationSpeedMult = 0f;
+            offSetX = -120f;
+            offSetY = -70f; //how much higher from the center the pet should float
+        }
+
+        public override void SetUpAuraAndGlowmask()
+        {
+            glowmaskTexture = "Projectiles/Pets/LightPets/SupJewel/Bishop_Glow";
+        }
+
+        public override void SetUpLight()
+        {
+            shouldLightUp = true;
+            RGB = new Vector3(207, 117, 56);
+            intensity = 2f;
+            abyssLightLevel = 5;
+        }
+
+        public override void SafeAI(Player player)
+        {
+            CalValEXPlayer modPlayer = player.GetModPlayer<CalValEXPlayer>();
+
+            if (player.dead)
+                modPlayer.SupJ = false;
+            if (modPlayer.SupJ)
+                projectile.timeLeft = 2;
+        }
+    }
+
+    public class SpookShark : FlyingPet
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Phantoshark");
+            Main.projFrames[projectile.type] = 8;
+            Main.projPet[projectile.type] = true;
+        }
+
+        public override void SafeSetDefaults()
+        {
+            projectile.width = 76;
+            projectile.height = 36;
             projectile.ignoreWater = true;
             facingLeft = true;
             spinRotation = false;
@@ -35,8 +164,8 @@ namespace CalValEX.Projectiles.Pets.LightPets.SupJewel
             inertia = 60f;
             animationSpeed = 12; //how fast the animation should play
             spinRotationSpeedMult = 0.2f;
-            offSetX = 48f * -Main.player[projectile.owner].direction; //this is needed so it's always behind the player.
-            offSetY = -50f; //how much higher from the center the pet should float
+            offSetX = 100f * -Main.player[projectile.owner].direction; //this is needed so it's always behind the player.
+            offSetY = -60f; //how much higher from the center the pet should float
         }
         public override void SetUpAuraAndGlowmask()
         {
@@ -64,16 +193,16 @@ namespace CalValEX.Projectiles.Pets.LightPets.SupJewel
             if (modPlayer.SupJ)
                 projectile.timeLeft = 2;
 
-            rotation += 0.1f;
+            rotation += 0.01f;
             if (extraScale > 0.25f && !yep)
                 yep = true;
-            if (extraScale < -0.10f && yep)
+            if (extraScale < 0f && yep)
                 yep = false;
 
             if (!yep)
-                extraScale += 0.01f;
+                extraScale += 0.005f;
             if (yep)
-                extraScale -= 0.01f;
+                extraScale -= 0.005f;
         }
 
         public override void SafePreDraw(SpriteBatch spriteBatch, Color lightColor)
@@ -115,12 +244,12 @@ namespace CalValEX.Projectiles.Pets.LightPets.SupJewel
         {
             distance[0] = 3400f; //teleport distance
             distance[1] = 640f; //faster speed distance
-            speed = 20f;
+            speed = 15f;
             inertia = 80f;
             animationSpeed = 12; //how fast the animation should play
             spinRotationSpeedMult = 0.2f;
-            offSetX = 16f * Main.player[projectile.owner].direction; //this is needed so it's always behind the player.
-            offSetY = 0f; //how much higher from the center the pet should float
+            offSetX = 80f * -Main.player[projectile.owner].direction; //this is needed so it's always behind the player.
+            offSetY = -20f; //how much higher from the center the pet should float
         }
 
         public override void SetUpAuraAndGlowmask()
@@ -133,64 +262,6 @@ namespace CalValEX.Projectiles.Pets.LightPets.SupJewel
             shouldLightUp = true;
             RGB = new Vector3(207, 117, 56);
             intensity = 1.25f;
-            abyssLightLevel = 5;
-        }
-
-        public override void SafeAI(Player player)
-        {
-            CalValEXPlayer modPlayer = player.GetModPlayer<CalValEXPlayer>();
-
-            if (player.dead)
-                modPlayer.SupJ = false;
-            if (modPlayer.SupJ)
-                projectile.timeLeft = 2;
-        }
-    }
-
-    public class Bishop : FlyingPet
-    {
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Dark Sun Elemental");
-            Main.projFrames[projectile.type] = 4;
-            Main.projPet[projectile.type] = true;
-        }
-
-        public override void SafeSetDefaults()
-        {
-            projectile.width = 100;
-            projectile.height = 174;
-            projectile.ignoreWater = true;
-            facingLeft = true;
-            spinRotation = false;
-            shouldFlip = false;
-            usesAura = false;
-            usesGlowmask = true;
-            auraUsesGlowmask = false;
-        }
-
-        public override void SetUpFlyingPet()
-        {
-            distance[0] = 3400f; //teleport distance
-            distance[1] = 640f; //faster speed distance
-            speed = 6f;
-            inertia = 70f;
-            animationSpeed = 12; //how fast the animation should play
-            spinRotationSpeedMult = 0.2f;
-            offSetX = 500f * -Main.player[projectile.owner].direction; //this is needed so it's always behind the player.
-            offSetY = 200f; //how much higher from the center the pet should float
-        }
-
-        public override void SetUpAuraAndGlowmask()
-        {
-            glowmaskTexture = "Projectiles/Pets/LightPets/SupJewel/Bishop_Glow";
-        }
-
-        public override void SetUpLight()
-        {
-            shouldLightUp = true;
-            RGB = new Vector3(207, 117, 56);
-            intensity = 2f;
             abyssLightLevel = 5;
         }
 
@@ -221,7 +292,7 @@ namespace CalValEX.Projectiles.Pets.LightPets.SupJewel
             projectile.ignoreWater = true;
             facingLeft = true;
             spinRotation = false;
-            shouldFlip = true;
+            shouldFlip = false;
             usesAura = false;
             usesGlowmask = true;
             auraUsesGlowmask = false;
@@ -231,12 +302,12 @@ namespace CalValEX.Projectiles.Pets.LightPets.SupJewel
         {
             distance[0] = 3400f; //teleport distance
             distance[1] = 640f; //faster speed distance
-            speed = 18f;
-            inertia = 70f;
+            speed = 20f;
+            inertia = 20f;
             animationSpeed = 12; //how fast the animation should play
-            spinRotationSpeedMult = 0.2f;
-            offSetX = 16f * -Main.player[projectile.owner].direction; //this is needed so it's always behind the player.
-            offSetY = 0f; //how much higher from the center the pet should float
+            spinRotationSpeedMult = 0f;
+            offSetX = 1f; //this is needed so it's always behind the player.
+            offSetY = 1f; //how much higher from the center the pet should float
         }
 
         public override void SetUpAuraAndGlowmask()
@@ -260,72 +331,6 @@ namespace CalValEX.Projectiles.Pets.LightPets.SupJewel
                 modPlayer.SupJ = false;
             if (modPlayer.SupJ)
                 projectile.timeLeft = 2;
-        }
-    }
-
-    public class NWings : ModProjectile
-    {
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Nightmare Fuel Wings");
-            Main.projFrames[projectile.type] = 4;
-            Main.projPet[projectile.type] = true;
-        }
-
-        public override void SetDefaults()
-        {
-            projectile.damage = 0;
-            projectile.friendly = true;
-            projectile.tileCollide = false;
-            projectile.ignoreWater = true;
-            projectile.hostile = false;
-            projectile.width = 200;
-            projectile.height = 200;
-            projectile.aiStyle = -1;
-            projectile.netImportant = true;
-        }
-
-        public override bool? CanCutTiles()
-        {
-            return false;
-        }
-
-        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
-        {
-            Lighting.AddLight(projectile.Center, new Vector3(0.541176471f, 1f, 1f));
-            Texture2D texture = Main.projectileTexture[projectile.type];
-            spriteBatch.Draw(texture, projectile.Center - Main.screenPosition, null, projectile.GetAlpha(lightColor), projectile.rotation, new Vector2(texture.Width / 2, texture.Height / 2), 1f, projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.None, 0f);
-            return false;
-        }
-
-        public override void AI()
-        {
-            Player player = Main.player[projectile.owner];
-
-            CalValEXPlayer modPlayer = player.GetModPlayer<CalValEXPlayer>();
-
-            if (player.dead)
-                modPlayer.SupJ = false;
-            if (modPlayer.SupJ)
-                projectile.timeLeft = 2;
-
-            Vector2 idlePosition = player.Center;
-            idlePosition.Y -= projectile.height / 2;
-            idlePosition.X -= projectile.width / 2;
-            projectile.position = idlePosition;
-            projectile.spriteDirection = 1;
-
-            projectile.frameCounter++;
-            if (projectile.frameCounter > 3)
-            {
-                projectile.frameCounter = 0;
-            }
-
-            Mod calamityMod = ModLoader.GetMod("CalamityMod");
-            if (calamityMod != null)
-            {
-                calamityMod.Call("AddAbyssLightStrength", projectile.owner, 2);
-            }
         }
     }
 }
