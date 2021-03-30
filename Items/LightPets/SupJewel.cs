8a6859cf85cce0b3a9aefa0dfdd84ec0a8a7b431
GinYuH <72181395+GinYuH@users.noreply.github.com>
2021-03-29 21:57:09 -0700

    aqua

--- Items/LightPets/SupJewel.cs
+++ Items/LightPets/SupJewel.cs
@@ -26,6 +26,25 @@ namespace CalValEX.Items.LightPets
             item.rare = ItemRarityID.Purple;
             item.buffType = mod.BuffType("SupJewelBuff");
         }
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            //rarity 12 (Turquoise) = new Color(0, 255, 200)
+            //rarity 13 (Pure Green) = new Color(0, 255, 0)
+            //rarity 14 (Dark Blue) = new Color(43, 96, 222)
+            //rarity 15 (Violet) = new Color(108, 45, 199)
+            //rarity 16 (Hot Pink/Developer) = new Color(255, 0, 255)
+            //rarity rainbow (no expert tag on item) = new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB)
+            //rarity rare variant = new Color(255, 140, 0)
+            //rarity dedicated(patron items) = new Color(139, 0, 0)
+            //look at https://calamitymod.gamepedia.com/Rarity to know where to use the colors
+            foreach (TooltipLine tooltipLine in tooltips)
+            {
+                if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
+                {
+                    tooltipLine.overrideColor = new Color(107, 240, 255); //change the color accordingly to above
+                }
+            }
+        }
 
         public override void UseStyle(Player player)
         {
@@ -57,26 +76,6 @@ namespace CalValEX.Items.LightPets
                 recipe.AddTile(TileID.LunarCraftingStation);
                 recipe.SetResult(this);
                 recipe.AddRecipe();
-            }
-        }
-
-        public override void ModifyTooltips(List<TooltipLine> tooltips)
-        {
-            //rarity 12 (Turquoise) = new Color(0, 255, 200)
-            //rarity 13 (Pure Green) = new Color(0, 255, 0)
-            //rarity 14 (Dark Blue) = new Color(43, 96, 222)
-            //rarity 15 (Violet) = new Color(108, 45, 199)
-            //rarity 16 (Hot Pink/Developer) = new Color(255, 0, 255)
-            //rarity rainbow (no expert tag on item) = new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB)
-            //rarity rare variant = new Color(255, 140, 0)
-            //rarity dedicated(patron items) = new Color(139, 0, 0)
-            //look at https://calamitymod.gamepedia.com/Rarity to know where to use the colors
-            foreach (TooltipLine tooltipLine in tooltips)
-            {
-                if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
-                {
-                    tooltipLine.overrideColor = new Color(255, 0, 255); //change the color accordingly to above
-                }
             }
         }
     }
