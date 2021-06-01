d12639f65957ed26cdb1e00157421c9716e77477
GinYuH <72181395+GinYuH@users.noreply.github.com>
2021-06-01 00:12:04 -0700

    Add files via upload

---
+++ Buffs/Pets/CoolBlueBuff.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace CalValEX.Buffs.Pets
+{
+    public class CoolBlueBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Cool Blue Dudes");
+            Description.SetDefault("Some very coul bleu doods");
+            Main.buffNoTimeDisplay[Type] = true;
+            Main.vanityPet[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.buffTime[buffIndex] = 18000;
+            player.GetModPlayer<CalValEXPlayer>().bSignut = true;
+            bool petProjectileNotSpawned = player.ownedProjectileCounts[ModContent.ProjectileType<Projectiles.Pets.CoolBlueSignut>()] <= 0;
+            if (petProjectileNotSpawned && player.whoAmI == Main.myPlayer)
+            {
+                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, ModContent.ProjectileType<Projectiles.Pets.CoolBlueSignut>(), 0, 0f, player.whoAmI, 0f, 0f);
+            }
+            player.buffTime[buffIndex] = 18000;
+            player.GetModPlayer<CalValEXPlayer>().bSlime = true;
+            bool petProjectileNotSpawnedf = player.ownedProjectileCounts[ModContent.ProjectileType<Projectiles.Pets.CoolBlueSlime>()] <= 0;
+            if (petProjectileNotSpawnedf && player.whoAmI == Main.myPlayer)
+            {
+                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, ModContent.ProjectileType<Projectiles.Pets.CoolBlueSlime>(), 0, 0f, player.whoAmI, 0f, 0f);
+            }
+        }
+    }
+}
