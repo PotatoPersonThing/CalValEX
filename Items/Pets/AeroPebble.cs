288e6b71769b4edd5e70617d45ec898639b07d39
GinYuH <72181395+GinYuH@users.noreply.github.com>
2021-05-31 23:47:01 -0700

    disc -> sphere

--- Items/Pets/AeroPebble.cs
+++ Items/Pets/AeroPebble.cs
@@ -11,7 +11,7 @@ namespace CalValEX.Items.Pets
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Aerialite Bubble");
-            Tooltip.SetDefault("An odd disc covered in light gel");
+            Tooltip.SetDefault("An odd sphere covered in light gel");
         }
 
         public override void SetDefaults()
