06e964898bb56f8cb2829ead4cb208b6aa170828
GinYuH <72181395+GinYuH@users.noreply.github.com>
2021-06-04 10:48:13 -0700

    welcome to hell

--- Projectiles/Pets/Sirember.cs
+++ Projectiles/Pets/Sirember.cs
@@ -1,9 +1,17 @@
 using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
 
 namespace CalValEX.Projectiles.Pets
 {
     public class Sirember : FlyingPet
     {
+        private bool theterror = false;
+
+        int apocalypse = 0;
+        int countdown = 0;
+        public bool entropy = false;
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Giant Decapitated Floating Siren Head");
@@ -13,8 +21,8 @@ namespace CalValEX.Projectiles.Pets
 
         public override void SafeSetDefaults() //SafeSetDefaults!!!
         {
-            projectile.width = 22;
-            projectile.height = 22;
+            projectile.width = 112;
+            projectile.height = 112;
             projectile.ignoreWater = true;
             /* you don't need to set these anymore!
             projectile.penetrate = -1;
@@ -35,9 +43,9 @@ namespace CalValEX.Projectiles.Pets
         {
             distance[0] = 1200f; //teleport distance
             distance[1] = 560f; //faster speed distance
-            speed = 12f;
-            inertia = 60f;
-            animationSpeed = 7; //how fast the animation should play
+            speed = (theterror? 0 : 12f);
+            inertia = (theterror? 0: 60f);
+            animationSpeed = (theterror ? 0 : 7); //how fast the animation should play
             spinRotationSpeedMult = 0.2f; //rotation speed multiplier, keep it positive for it to spin in the right direction
             offSetX = 54f * -Main.player[projectile.owner].direction; //this is needed so it's always behind the player.
             offSetY = -130f; //how much higher from the center the pet should float
@@ -73,6 +81,61 @@ namespace CalValEX.Projectiles.Pets
              *
              * you can still use this, changing thing inside (however it's not recomended unless you want to add custom behaviour to this)
              */
+            /*countdown++;
+            if (!theterror)
+            {
+                if (countdown == 60)
+                {
+                    theterror = true;
+                    countdown = 666;
+                    Projectile.NewProjectile(projectile.position.X, projectile.position.Y, 0, 0, ModContent.ProjectileType<SiremberSpook>(), 0, 0, projectile.owner, 0);
+                }
+            }
+            if (theterror)
+            {
+                for (int j = 0; j < 1; j++)
+                {
+                    theterror = true;
+                    projectile.frame = 0;
+                    projectile.rotation = 0;
+                    //projectile.position = projectile.position;
+                }
+                apocalypse++;
+                if (apocalypse == 660)
+                {
+                    theterror = false;
+                    countdown = 0;
+                    apocalypse = -1;
+                }
+                else if (apocalypse == 300)
+                {
+                    player.AddBuff(BuffID.Blackout, 360);
+                }
+            }*/
+            apocalypse++;
+
+            if (apocalypse >= 23600 && apocalypse < 24260)
+            {
+                if (!entropy)
+                {
+                    entropy = true;
+                    Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/ReaperEnragedRoar"));
+                    Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, 0, 0, ModContent.ProjectileType<SiremberSpook>(), 0, 0, projectile.owner, 0);
+                    projectile.alpha = 255;
+                }
+
+            }
+            if (apocalypse >= 24260)
+            {
+                projectile.alpha = 0;
+                for (int x = 0; x < 60; x++)
+                {
+                    Dust dust;
+                    dust = Main.dust[Terraria.Dust.NewDust(projectile.Center, 30, 30, 16, 0f, 0f, 0, new Color(255, 255, 255), 1.644737f)];
+                }
+                apocalypse = 0;
+                entropy = false;
+            }
         }
     }
 }
---
+++ Projectiles/Pets/SiremberSpook.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalValEX.Projectiles.Pets
+{
+    public class SiremberSpook : ModProjectile
+    {
+        private bool theterror = false;
+
+        int apocalypse = 0;
+        int countdown = 0;
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Giant Decapitated Floating Siren Head");
+            Main.projFrames[projectile.type] = 6; //frames
+        }
+
+        public override string Texture => "CalValEX/Projectiles/Pets/Sirember";
+
+        public override void SetDefaults() 
+        {
+            projectile.width = 112;
+            projectile.height = 112;
+            projectile.ignoreWater = true;
+            projectile.penetrate = -1;
+            projectile.netImportant = true;
+            projectile.timeLeft *= 5;
+            projectile.friendly = true;
+            projectile.tileCollide = false;
+            projectile.aiStyle = -1;
+        }
+
+        public override void AI()
+        {
+            
+            Player player = Main.LocalPlayer;
+            CalValEXPlayer modPlayer = player.GetModPlayer<CalValEXPlayer>();
+
+            countdown++;
+
+            if (projectile.frame != 1)
+            {
+                projectile.frame = 1;
+            }
+            projectile.rotation = 0;
+            if (countdown == 660)
+            { 
+                projectile.Kill();
+
+            }
+            else if (countdown == 600)
+            {
+                player.AddBuff(BuffID.Obstructed, 60);
+            }
+            else if (countdown == 300)
+            {
+                player.AddBuff(BuffID.Blackout, 360);
+                player.AddBuff(BuffID.Darkness, 360);
+            }
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            for (int x = 0; x < 60; x++)
+            {
+                Dust dust;
+                dust = Main.dust[Terraria.Dust.NewDust(projectile.Center, 30, 30, 16, 0f, 0f, 0, new Color(255, 255, 255), 1.644737f)];
+            }
+        }
+    }
+}
