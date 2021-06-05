904e9544e91c88d189e573f878f00bb243599cdf
GinYuH <72181395+GinYuH@users.noreply.github.com>
2021-06-05 00:05:03 -0700

    moss wall

---
+++ Items/Tiles/Walls/MossyGravelWall.cs
@@ -0,0 +1,40 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using CalValEX.Items.Tiles.Blocks;
+
+namespace CalValEX.Items.Tiles.Walls
+{
+    public class MossyGravelWall : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Mossy Abyss Gravel Wall");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 12;
+            item.height = 12;
+            item.maxStack = 999;
+            item.rare = 0;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 7;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = true;
+            item.createWall = ModContent.WallType<MossyGravelWallPlaced>();
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            Mod calamityMod = ModLoader.GetMod("CalamityMod");
+            recipe.AddIngredient(ModLoader.GetMod("CalamityMod").ItemType("AbyssGravelWallItem"), 4);
+            recipe.AddIngredient(ModLoader.GetMod("CalamityMod").ItemType("PlantyMush"), 1);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this, 4);
+            recipe.AddRecipe();
+        }
+    }
+}
