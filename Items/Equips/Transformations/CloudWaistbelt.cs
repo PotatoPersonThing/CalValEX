b93389fb18108df9b5d34d5824266d4dfcaa2bfd
GinYuH <72181395+GinYuH@users.noreply.github.com>
2021-06-14 05:31:26 -0700

    Valkyrian Garments

---
+++ Items/Equips/Transformations/CloudWaistbelt.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalValEX.Items.Equips.Transformations
+{
+	public class CloudWaistbelt : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Valkyrian Garments");
+			Tooltip.SetDefault("Surrounds the wearer with clouds");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 24;
+			item.height = 28;
+			item.accessory = true;
+			item.rare = 4;
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+			CalValEXPlayer p = player.GetModPlayer<CalValEXPlayer>();
+			p.cloudTrans = true;
+			if (hideVisual)
+			{
+				p.cloudHide = true;
+			}
+		}
+	}
+
+	public class CloudHead : EquipTexture
+	{
+		public override bool DrawHead()
+		{
+			return false;
+		}
+	}
+
+	public class CloudBody : EquipTexture
+	{
+		public override bool DrawBody()
+		{
+			return false;
+		}
+	}
+
+	public class CloudLegs : EquipTexture
+	{
+		public override bool DrawLegs()
+		{
+			return false;
+		}
+	}
+}
