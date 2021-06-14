da1b3954a4ed3e6651a3feb219126521c7d3100e
GinYuH <72181395+GinYuH@users.noreply.github.com>
2021-06-14 04:38:16 -0700

    Add files via upload

---
+++ Buffs/Transformations/SignutTransformationBuff.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace CalValEX.Buffs.Transformations
+{
+	public class SignutTransformationBuff : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Ethereal Assassin");
+			Description.SetDefault("Never been more edgy");
+			Main.debuff[Type] = true;
+			Main.buffNoSave[Type] = true;
+			Main.buffNoTimeDisplay[Type] = true;
+			canBeCleared = false;
+		}
+	}
+}
