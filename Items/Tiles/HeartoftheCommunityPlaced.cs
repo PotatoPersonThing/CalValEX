c20b2eaef5bbdd61bc7a73efddbb0a9e6da9b08b
GinYuH <72181395+GinYuH@users.noreply.github.com>
2021-06-15 00:09:23 -0700

    added heart of the community

---
+++ Items/Tiles/HeartoftheCommunityPlaced.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using static Terraria.ModLoader.ModContent;
+using Terraria.Graphics.Shaders;
+
+namespace CalValEX.Items.Tiles
+{
+    internal class HeartoftheCommunityPlaced : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLighted[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true; // Necessary since Style3x3Wall uses AnchorWall
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+            TileObjectData.newTile.Width = 4;
+            TileObjectData.newTile.Height = 4;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16 }; //
+
+            animationFrameHeight = 72;
+            TileObjectData.addTile(Type);
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Heart of the Community");
+            AddMapEntry(new Color(139, 0, 0), name);
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 48, 48, ItemType<HeartoftheCommunity>());
+        }
+
+        public override void AnimateTile(ref int frame, ref int frameCounter)
+        {
+            frameCounter++;
+            if (frameCounter > 6) //make this number lower/bigger for faster/slower animation
+            {
+                frameCounter = 0;
+                frame++;
+                if (frame > 9)
+                {
+                    frame = 0;
+                }
+            }
+        }
+    }
+}
