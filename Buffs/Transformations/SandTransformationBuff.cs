d248b59c67fa6c4385f2ffbdba6f22b063370ddc
GinYuH <72181395+GinYuH@users.noreply.github.com>
2021-06-14 22:07:15 -0700

    Add files via upload

--- Buffs/Transformations/SandTransformationBuff.cs
+++ Buffs/Transformations/SandTransformationBuff.cs
@@ -1,28 +1,18 @@
-﻿/*
-using Terraria;
+﻿using Terraria;
 using Terraria.ModLoader;
 
 namespace CalValEX.Buffs.Transformations
 {
-    public class SandTransformationBuff : ModBuff
-    {
-        public override void SetDefaults()
-        {
-            DisplayName.SetDefault("Sand Transformation");
-            Main.debuff[Type] = true;
-            Main.buffNoSave[Type] = true;
-            Main.buffNoTimeDisplay[Type] = true;
-            canBeCleared = false;
-        }
-
-        public override void Update(Player player, ref int buffIndex)
-        {
-            if (!player.GetModPlayer<CalValEXPlayer>().sandTPrevious)
-            {
-                player.DelBuff(buffIndex);
-                buffIndex--;
-            }
-        }
-    }
+	public class SandTransformationBuff : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Sand Elemental Transformation");
+			Description.SetDefault("heh.");
+			Main.debuff[Type] = true;
+			Main.buffNoSave[Type] = true;
+			Main.buffNoTimeDisplay[Type] = true;
+			canBeCleared = false;
+		}
+	}
 }
-*/
