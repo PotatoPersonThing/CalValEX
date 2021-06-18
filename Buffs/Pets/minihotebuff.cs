c00b7d1630bcf2e4f9db9ccc746c08e497da6965
GinYuH <72181395+GinYuH@users.noreply.github.com>
2021-06-17 19:37:41 -0700

    Add files via upload

--- Buffs/Pets/minihotebuff.cs
+++ Buffs/Pets/minihotebuff.cs
@@ -17,24 +17,38 @@ namespace CalValEX.Buffs.Pets
         {
             player.buffTime[buffIndex] = 18000;
             player.GetModPlayer<CalValEXPlayer>().rarebrimling = true;
-            player.GetModPlayer<CalValEXPlayer>().raresandmini = true;
-            player.GetModPlayer<CalValEXPlayer>().cloudmini = true;
-            player.GetModPlayer<CalValEXPlayer>().babywaterclone = true;
-            player.GetModPlayer<CalValEXPlayer>().cloudmini = true;
-            player.GetModPlayer<CalValEXPlayer>().sandmini = true;
-            bool petProjectileNotSpawned = (player.ownedProjectileCounts[mod.ProjectileType("rarebrimling")] <= 0 &&
-                                            player.ownedProjectileCounts[mod.ProjectileType("raresandmini")] <= 0 &&
-                                            player.ownedProjectileCounts[mod.ProjectileType("sandmini")] <= 0 &&
-                                            player.ownedProjectileCounts[mod.ProjectileType("cloudmini")] <= 0 &&
-                                            player.ownedProjectileCounts[mod.ProjectileType("babywaterclone")] <= 0
-                                           );
+            bool petProjectileNotSpawned = player.ownedProjectileCounts[ModContent.ProjectileType<Projectiles.Pets.rarebrimling>()] <= 0;
             if (petProjectileNotSpawned && player.whoAmI == Main.myPlayer)
             {
-                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, mod.ProjectileType("rarebrimling"), 0, 0f, player.whoAmI, 0f, 0f);
-                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, mod.ProjectileType("raresandmini"), 0, 0f, player.whoAmI, 0f, 0f);
-                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, mod.ProjectileType("sandmini"), 0, 0f, player.whoAmI, 0f, 0f);
-                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, mod.ProjectileType("cloudmini"), 0, 0f, player.whoAmI, 0f, 0f);
-                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, mod.ProjectileType("babywaterclone"), 0, 0f, player.whoAmI, 0f, 0f);
+                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, ModContent.ProjectileType<Projectiles.Pets.rarebrimling>(), 0, 0f, player.whoAmI, 0f, 0f);
+            }
+            player.buffTime[buffIndex] = 18000;
+            player.GetModPlayer<CalValEXPlayer>().cloudmini = true;
+            bool petProjectileNotSpawnedf = player.ownedProjectileCounts[ModContent.ProjectileType<Projectiles.Pets.cloudmini>()] <= 0;
+            if (petProjectileNotSpawnedf && player.whoAmI == Main.myPlayer)
+            {
+                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, ModContent.ProjectileType<Projectiles.Pets.cloudmini>(), 0, 0f, player.whoAmI, 0f, 0f);
+            }
+            player.buffTime[buffIndex] = 18000;
+            player.GetModPlayer<CalValEXPlayer>().raresandmini = true;
+            bool petProjectileNotSpawnedfl = player.ownedProjectileCounts[ModContent.ProjectileType<Projectiles.Pets.raresandmini>()] <= 0;
+            if (petProjectileNotSpawnedfl && player.whoAmI == Main.myPlayer)
+            {
+                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, ModContent.ProjectileType<Projectiles.Pets.raresandmini>(), 0, 0f, player.whoAmI, 0f, 0f);
+            }
+            player.buffTime[buffIndex] = 18000;
+            player.GetModPlayer<CalValEXPlayer>().sandmini = true;
+            bool petProjectileNotSpawnedf2 = player.ownedProjectileCounts[ModContent.ProjectileType<Projectiles.Pets.sandmini>()] <= 0;
+            if (petProjectileNotSpawnedf2 && player.whoAmI == Main.myPlayer)
+            {
+                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, ModContent.ProjectileType<Projectiles.Pets.sandmini>(), 0, 0f, player.whoAmI, 0f, 0f);
+            }
+            player.buffTime[buffIndex] = 18000;
+            player.GetModPlayer<CalValEXPlayer>().babywaterclone = true;
+            bool petProjectileNotSpawnedf3 = player.ownedProjectileCounts[ModContent.ProjectileType<Projectiles.Pets.babywaterclone>()] <= 0;
+            if (petProjectileNotSpawnedf3 && player.whoAmI == Main.myPlayer)
+            {
+                Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, ModContent.ProjectileType<Projectiles.Pets.babywaterclone>(), 0, 0f, player.whoAmI, 0f, 0f);
             }
         }
     }
