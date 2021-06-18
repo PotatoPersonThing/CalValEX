07c9a4f2320c5307b37f48e0d11d2a4b1a7d88ad
GinYuH <72181395+GinYuH@users.noreply.github.com>
2021-06-17 19:37:20 -0700

    mini heart fixes

--- Items/Pets/Minihote.cs
+++ Items/Pets/Minihote.cs
@@ -19,12 +19,14 @@ namespace CalValEX.Items.Pets
 
         public override void SetDefaults()
         {
+            item.CloneDefaults(ItemID.ZephyrFish);
             item.UseSound = SoundID.NPCHit5;
             item.shoot = mod.ProjectileType("rarebrimling");
             item.value = Item.sellPrice(0, 4, 0, 0);
             item.expert = true;
             item.rare = 10;
             item.buffType = mod.BuffType("minihotebuff");
+            item.noUseGraphic = true;
         }
 
         public override void UseStyle(Player player)
@@ -33,6 +35,19 @@ namespace CalValEX.Items.Pets
             {
                 player.AddBuff(item.buffType, 3600, true);
             }
+        }
+        public override bool Shoot(Player player, ref Microsoft.Xna.Framework.Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            type = mod.ProjectileType("rarebrimling");
+            return base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
+            type = mod.ProjectileType("cloudmini");
+            return base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
+            type = mod.ProjectileType("babywaterclone");
+            return base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
+            type = mod.ProjectileType("sandmini");
+            return base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
+            type = mod.ProjectileType("raresandmini");
+            return base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
         }
 
         public override void AddRecipes()
