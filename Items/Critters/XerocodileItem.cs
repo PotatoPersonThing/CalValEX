0850d21661786dad802182cae497078aa0b0f604
GinYuH <>
2021-08-22 21:07:45 -0700

    10 xerocodiles

---
+++ Items/Critters/XerocodileItem.cs
@@ -0,0 +1,63 @@
+﻿using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using CalValEX.Items.Critters;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria.ID;
+
+namespace CalValEX.Items.Critters
+{
+    internal class XerocodileItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Awakened Xerocodile");
+        }
+
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.autoReuse = true;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.maxStack = 999;
+            item.consumable = true;
+            item.width = 22;
+            item.height = 18;
+            item.noUseGraphic = true;
+            item.rare = 10;
+            item.makeNPC = (short)NPCType<Xerocodile>();
+        }
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            //rarity 12 (Turquoise) = new Color(0, 255, 200)
+            //rarity 13 (Pure Green) = new Color(0, 255, 0)
+            //rarity 14 (Dark Blue) = new Color(43, 96, 222)
+            //rarity 15 (Violet) = new Color(108, 45, 199)
+            //rarity 16 (Hot Pink/Developer) = new Color(255, 0, 255)
+            //rarity rainbow (no expert tag on item) = new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB)
+            //rarity rare variant = new Color(255, 140, 0)
+            //rarity dedicated(patron items) = new Color(139, 0, 0)
+            //look at https://calamitymod.gamepedia.com/Rarity to know where to use the colors
+            foreach (TooltipLine tooltipLine in tooltips)
+            {
+                if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
+                {
+                    tooltipLine.overrideColor = new Color(0, 255, 0); //change the color accordingly to above
+                }
+            }
+        }
+        public override void AddRecipes()
+        {
+            Mod clamMod = ModLoader.GetMod("CalamityMod");
+            {
+                ModRecipe recipe = new ModRecipe(mod);
+                recipe.AddIngredient(clamMod.ItemType("XerocodileItem"), 10);
+                recipe.AddIngredient(clamMod.ItemType("BloodstoneCore"), 1);
+                recipe.SetResult(this);
+                recipe.AddRecipe();
+            }
+        }
+    }
+}
