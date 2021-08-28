acd391bb63f457ba6892098f320eb5d07f227b68
GinYuH <>
2021-08-27 20:50:23 -0700

    a

--- NPCs/JellyPriest/JellyPriestNPC.cs
+++ NPCs/JellyPriest/JellyPriestNPC.cs
@@ -61,7 +61,7 @@ namespace CalValEX.NPCs.JellyPriest
 
         public override bool CanTownNPCSpawn(int numTownNPCs, int money)
         {
-            if (((bool)ModLoader.GetMod("CalamityMod").Call("GetBossDowned", "acidrainscourge") || CalValEXWorld.rescuedjelly) && !CalValEXConfig.Instance.TownNPC)
+            if (CalValEXWorld.rescuedjelly && !CalValEXConfig.Instance.TownNPC)
             {
                 jellyspawn = true;
             }
