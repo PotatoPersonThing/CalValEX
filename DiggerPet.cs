8e97c265ad6eb816b3087652370ab5749a59f59e
Drædon Gaming <lockout18yt@gmail.com>
2022-03-11 19:36:51 -0600

    Worm light code
    Small update to the digger + support for light on Iban's worm code

--- DiggerPet.cs
+++ DiggerPet.cs
@@ -13,7 +13,7 @@ namespace CalValEX.Projectiles.Pets.LightPets
 {
     public class DiggerPet : BaseWormPet
     {
-        public override string Texture => "CalValEX/Projectiles/Pets/LightPets/DiggerHead"; 
+        public override string Texture => "CalValEX/Projectiles/Pets/LightPets/DiggerHead";
         public override WormPetVisualSegment HeadSegment() => new WormPetVisualSegment("CalValEX/Projectiles/Pets/LightPets/DiggerHead", true, 1, 1);
         public override WormPetVisualSegment BodySegment() => new WormPetVisualSegment("CalValEX/Projectiles/Pets/LightPets/DiggerSegment", true, 1, 1);
         public override WormPetVisualSegment TailSegment() => new WormPetVisualSegment("CalValEX/Projectiles/Pets/LightPets/DiggerTail", true, 1, 1);
@@ -26,7 +26,7 @@ namespace CalValEX.Projectiles.Pets.LightPets
         public override float WanderDistance => Owner.GetModPlayer<CalValEXPlayer>().RepairBot ? 160 : 60;
         public override float GetSpeed => MathHelper.Lerp(
             Owner.GetModPlayer<CalValEXPlayer>().RepairBot ? 20 : 10,
-            Owner.GetModPlayer<CalValEXPlayer>().RepairBot ? 40 : 16, 
+            Owner.GetModPlayer<CalValEXPlayer>().RepairBot ? 40 : 16,
             MathHelper.Clamp(projectile.Distance(IdealPosition) / (WanderDistance * 2.2f) - 1f, 0, 1));
 
         public override int BodyVariants => 1;
@@ -39,13 +39,15 @@ namespace CalValEX.Projectiles.Pets.LightPets
             Main.projPet[projectile.type] = true;
             ProjectileID.Sets.LightPet[projectile.type] = true;
         }
+        public override void SetUpLight()
+        {
+            shouldLightUp = true;
+            RGB = new Vector3(1, 0.494117647f, 0.125f);
+            intensity = 1.3f;
+            abyssLightLevel = 2;
+        }
         public override void MoveTowardsIdealPosition()
         {
-            /*If the owner is holding right click, shift its goal from the worms ideal position tothe mouse cursor
-            if (Owner.Calamity().mouseRight && Owner.HeldItem.type == ModContent.ItemType<GunmetalRemote>())
-                RelativeIdealPosition = Owner.Calamity().mouseWorld - Owner.Center;
-            */
-
             //Rotate towards its ideal position
             projectile.rotation = projectile.rotation.AngleTowards((IdealPosition - projectile.Center).ToRotation(), MathHelper.Lerp(MaximumSteerAngle, MinimumSteerAngle, MathHelper.Clamp(projectile.Distance(IdealPosition) / 80f, 0, 1)));
             projectile.velocity = projectile.rotation.ToRotationVector2() * GetSpeed;
