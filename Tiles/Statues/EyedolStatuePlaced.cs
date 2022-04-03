a2d2372cfe37375ef9eed6f1c83fa1329b8c7124
GinYuH <>
2022-04-02 20:59:20 -0700

    more new shirt sheets

--- Tiles/Statues/EyedolStatuePlaced.cs
+++ Tiles/Statues/EyedolStatuePlaced.cs
@@ -34,7 +34,6 @@ namespace CalValEX.Tiles.Statues
 
         public override void HitWire(int i, int j)
         {
-            // Find the coordinates of top left tile square through math
             int y = j - Main.tile[i, j].TileFrameY / 18;
             int x = i - Main.tile[i, j].TileFrameX / 18;
 
@@ -45,25 +44,18 @@ namespace CalValEX.Tiles.Statues
             Wiring.SkipWire(x + 1, y + 1);
             Wiring.SkipWire(x + 1, y + 2);
 
-            // We add 16 to x to spawn right between the 2 tiles. We also want to right on the ground in the y direction.
             int spawnX = x * 16 + 16;
             int spawnY = (y + 3) * 16;
 
-            // This example shows both item spawning code and npc spawning code, you can use whichever code suits your mod
-
-            // If you want to make an NPC spawning statue, see below.
             int npcIndex = -1;
-            // 30 is the time before it can be used again. NPC.MechSpawn checks nearby for other spawns to prevent too many spawns. 3 in immediate vicinity, 6 nearby, 10 in world.
-            if (Wiring.CheckMech(x, y, 30) && NPC.MechSpawn((float)spawnX, (float)spawnY, (ModContent.NPCType<Eyedol>())))
+            if (Wiring.CheckMech(x, y, 30) && NPC.MechSpawn((float)spawnX, (float)spawnY, ModContent.NPCType<Eyedol>()))
             {
-                npcIndex = NPC.NewNPC(new Terraria.DataStructures.EntitySource_SpawnNPC(),spawnX, spawnY - 12, (ModContent.NPCType<Eyedol>()));
+                npcIndex = NPC.NewNPC(new Terraria.DataStructures.EntitySource_TileBreak(i, j), spawnX, spawnY - 12, ModContent.NPCType<Eyedol>());
             }
             if (npcIndex >= 0)
             {
                 Main.npc[npcIndex].value = 0f;
                 Main.npc[npcIndex].npcSlots = 0f;
-                // Prevents Loot if NPCID.Sets.NoEarlymodeLootWhenSpawnedFromStatue and !Main.HardMode or NPCID.Sets.StatueSpawnedDropRarity != -1 and NextFloat() >= NPCID.Sets.StatueSpawnedDropRarity or killed by traps.
-                // Prevents CatchNPC
                 Main.npc[npcIndex].SpawnedFromStatue = true;
             }
         }
