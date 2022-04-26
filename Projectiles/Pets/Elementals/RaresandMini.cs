c05b7edb1a450cbfd7f10bc0f4ef2f8c36f0cf3d
GinYuH <>
2022-04-26 08:04:32 -0700

    readded mini elementals

---
+++ Projectiles/Pets/Elementals/RaresandMini.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+
+namespace CalValEX.Projectiles.Pets.Elementals
+{
+    public class RaresandMini : ModFlyingPet
+    {
+        public override float TeleportThreshold => 1200f;
+
+        public override Vector2 FlyingOffset => new Vector2(90f * -Main.player[Projectile.owner].direction, -50f);
+
+        public override void SetStaticDefaults()
+        {
+            PetSetStaticDefaults(lightPet: false);
+            DisplayName.SetDefault("Dustgirl");
+            Main.projFrames[Projectile.type] = 6;
+        }
+
+        public override void SetDefaults()
+        {
+            PetSetDefaults();
+            Projectile.width = 22;
+            Projectile.height = 22;
+            Projectile.ignoreWater = true;
+        }
+
+        public override void PetFunctionality(Player player)
+        {
+            CalValEXPlayer modPlayer = player.GetModPlayer<CalValEXPlayer>();
+
+            if (player.dead)
+                modPlayer.raresandmini = false;
+
+            if (modPlayer.raresandmini)
+                Projectile.timeLeft = 2;
+        }
+
+        public override void Animation(int state)
+        {
+            SimpleAnimation(speed: 7);
+        }
+    }
+}
---
+++ Projectiles/Pets/Elementals/Sandmini.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+
+namespace CalValEX.Projectiles.Pets.Elementals
+{
+    public class Sandmini : ModFlyingPet
+    {
+        public override float TeleportThreshold => 1200f;
+
+        public override Vector2 FlyingOffset => new Vector2(-90f * -Main.player[Projectile.owner].direction, -50f);
+
+        public override void SetStaticDefaults()
+        {
+            PetSetStaticDefaults(lightPet: false);
+            DisplayName.SetDefault("dustgirl");
+            Main.projFrames[Projectile.type] = 6;
+        }
+
+        public override void SetDefaults()
+        {
+            PetSetDefaults();
+            Projectile.width = 22;
+            Projectile.height = 22;
+            Projectile.ignoreWater = true;
+        }
+
+        public override void PetFunctionality(Player player)
+        {
+            CalValEXPlayer modPlayer = player.GetModPlayer<CalValEXPlayer>();
+
+            if (player.dead)
+                modPlayer.sandmini = false;
+            if (modPlayer.sandmini)
+                Projectile.timeLeft = 2;
+        }
+
+        public override void Animation(int state)
+        {
+            SimpleAnimation(speed: 7);
+        }
+    }
+}
