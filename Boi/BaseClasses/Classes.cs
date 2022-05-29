ad2e0fe6f1035d451cb1d24022b989cc51d4a1e4
GinYuH <>
2022-05-28 22:22:08 -0700

    Iban save me (this commit is broken)

---
+++ Boi/BaseClasses/Classes.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+
+namespace CalValEX.Boi.BaseClasses
+{
+    public class BoiEntity
+    {
+        public Vector2 OldPosition;
+        public Vector2 Position;
+        public Vector2 Velocity;
+        public List<BoiItem> Inventory;
+        public virtual void OnSpawn() { }
+        public virtual void Update() { }
+    }
+
+    public class BoiRoom
+    {
+
+    }
+
+    public class BoiItem
+    {
+        Entity Owner;
+
+        public void UpdateEffect() { }
+    }
+}
