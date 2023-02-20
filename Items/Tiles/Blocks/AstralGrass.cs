98722aa8dcc0f424040c0fa9ac71ee4a8d69341f
GinYuH <>
2023-02-20 06:15:07 -0800

    grass and gas

--- Items/Tiles/Blocks/AstralGrass.cs
+++ Items/Tiles/Blocks/AstralGrass.cs
@@ -1,28 +1,44 @@
-﻿using Terraria.ModLoader;
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
 
 namespace CalValEX.Items.Tiles.Blocks
 {
     public class AstralGrass : ModItem
-    {
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Astral Grass");
-            SacrificeTotal = 100;
-        }
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Blighted Astral Grass Seeds");
+			Tooltip.SetDefault("Places grass on blighted astral dirt");
+		}
 
-        public override void SetDefaults()
-        {
-            Item.useStyle = 1;
-            Item.useTurn = true;
-            Item.useAnimation = 15;
-            Item.useTime = 10;
-            Item.autoReuse = true;
-            Item.maxStack = 99;
-            Item.consumable = true;
-            Item.width = 16;
-            Item.height = 28;
-            Item.rare = 0;
-            Item.createTile = ModContent.TileType<CalamityMod.Tiles.Astral.AstralGrass>();
-        }
+		public override void SetDefaults()
+		{
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.consumable = true;
+			Item.width = Item.height = 16;
+			Item.useTime = 15;
+			Item.useAnimation = 15;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.maxStack = 999;
+		}
+
+		public override bool? UseItem(Player player)
+		{
+			Tile tile = Framing.GetTileSafely(Player.tileTargetX, Player.tileTargetY);
+			
+			if (tile.HasTile && tile.TileType == ModContent.TileType<Tiles.AstralBlocks.AstralDirtPlaced>() && player.IsInTileInteractionRange(Player.tileTargetX, Player.tileTargetY))
+			{
+				Main.tile[Player.tileTargetX, Player.tileTargetY].TileType = (ushort)ModContent.TileType<Tiles.AstralBlocks.AstralGrassPlaced>();
+
+				SoundEngine.PlaySound(SoundID.Dig, player.Center);
+
+				return true;
+			}
+
+			return false;
+		}
     }
 }
