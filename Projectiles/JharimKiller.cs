1fe97a9adfd3d831e4026bbf551ff2b5889b2540
GinYuH <>
2023-05-21 18:40:03 -0700

    calamity fixes

--- Projectiles/JharimKiller.cs
+++ Projectiles/JharimKiller.cs
@@ -43,7 +43,7 @@ namespace CalValEX.Projectiles
             return false;
         }
 
-        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
         {
             if (target.type == ModContent.NPCType<AprilFools.Jharim.Jharim>() && target.life <= 0)
             {
