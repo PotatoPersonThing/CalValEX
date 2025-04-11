3608cccc6f63527dc58827bede3c3506dd172496
GinYuH <72181395+GinYuH@users.noreply.github.com>
2025-04-11 15:01:15 -0700

    plushies can now be placed on beds

--- Items/Plushies/PlushManager.cs
+++ Items/Plushies/PlushManager.cs
@@ -10,6 +10,8 @@ using CalValEX.Projectiles.Plushies;
 using Terraria.DataStructures;
 using Terraria.Audio;
 using System.IO;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
 
 namespace CalValEX.Items.Plushies
 {
@@ -322,17 +324,35 @@ namespace CalValEX.Items.Plushies
             TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
             TileObjectData.newTile.Width = Width;
             TileObjectData.newTile.Height = Height;
+            TileObjectData.newTile.AnchorBottom = new AnchorData(Terraria.Enums.AnchorType.SolidTile | Terraria.Enums.AnchorType.SolidWithTop | Terraria.Enums.AnchorType.AlternateTile, TileObjectData.newTile.Width, 0);
             List<int> heightArray = new List<int>(0);
             for (int i = 0; i < Height; i++)
             {
                 heightArray.Add(16);
             }
-            TileObjectData.newTile.CoordinateHeights = heightArray.ToArray();
+            TileObjectData.newTile.CoordinateHeights = [.. heightArray];
             TileObjectData.addTile(Type);
             LocalizedText name = CreateMapEntryName();
             AddMapEntry(new Color(144, 148, 144), name);
             DustType = 11;
         }
+
+        public override void PostSetupTileMerge()
+        {
+            // Allow plushies to be placed on any sleepable bed
+            List<int> beds = new List<int>();
+            for (int i = 0; i < TileLoader.TileCount; i++)
+            {
+                if (TileID.Sets.CanBeSleptIn[i])
+                {
+                    beds.Add(i);
+                }
+            }
+            int[] bedArray = [.. beds];
+            TileObjectData data = TileObjectData.GetTileData(Type, 0);
+            data.AnchorAlternateTiles = bedArray;
+        }
+
         public override void MouseOver(int i, int j)
         {
             Player localPlayer = Main.LocalPlayer;
@@ -389,6 +409,39 @@ namespace CalValEX.Items.Plushies
             }
             NetMessage.SendTileSquare(-1, x + 1, y + 1, Width);
             SoundEngine.PlaySound(ClickSound, new Vector2(i * 16, j * 16));
+        }
+
+        public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+            Tile t = Framing.GetTileSafely(i, j);
+            // Get the top left tile
+            int tileX = t.TileFrameX / 18;
+            while (tileX >= 4)
+            {
+                tileX -= 4;
+            }
+            tileX = i - tileX;
+            int tileY = t.TileFrameY / 18;
+            while (tileY >= 4)
+            {
+                tileY -= 4;
+            }
+            tileY = j - tileY;
+
+            // If the tile beneath can be slept in (aka a bed most of the time), run
+            if (Main.tile[tileX, tileY + Height].HasTile && TileID.Sets.CanBeSleptIn[Main.tile[tileX ,tileY + Height].TileType])
+            {
+                // Don't draw for tiles besides the top left
+                if (t.TileFrameX == 0 && t.TileFrameY == 0)
+                {
+                    Texture2D tex = TextureAssets.Item[ItemType].Value;
+                    spriteBatch.Draw(tex, new Vector2(i, j) * 16 - Main.screenPosition + new Vector2(16, 16 * Height + 16) + new Vector2(Main.offScreenRange, Main.offScreenRange), null, Lighting.GetColor(i, j), 0, new Vector2(tex.Width / 2, tex.Height), 1, 0, 0);
+                }
+                // Regardless of the above, cancel drawing
+                return false;
+            }
+            // If there's no bed, draw normally
+            return true;
         }
     }
 
